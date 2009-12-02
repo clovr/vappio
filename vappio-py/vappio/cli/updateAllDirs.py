261099dd04691a19f79eb4e643a5cd37545fe902
orbitz <orbitz@gmail.com>
2009-12-02 18:02:08 +0000

    Adding scripts to help cut a release

---
+++ vappio-py/vappio/cli/updateAllDirs.py
@@ -0,0 +1,18 @@
+##
+# This updates every directory with the latest from SVN.
+from igs.utils.commands import runSystemEx
+
+
+def main(options):
+    runSystemEx("""svn export --force https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/stow /usr/local/stow""")
+    runSystemEx("""svn export --force https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/opt-packages /opt/opt-packages""")
+    runSystemEx("""svn export --force https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/config_policies /opt/config_policies""")
+    runSystemEx("""svn export --force https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/vappio-py /opt/vappio-py""")
+    runSystemEx("""svn export --force https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/vappio-scripts /opt/vappio-scripts""")
+
+
+if __name__ == '__main__':
+    ##
+    # Just call it with None for now, will handle options later
+    main(None)
+    
