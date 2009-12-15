a11cf9af886a897cf1e660f87b4ca707d8294a3e
Cesar Arze <cesar.arze@gmail.com>
2009-12-15 15:26:44 +0000

    Added submit_dir_for_harvesting.sh

---
+++ vappio-scripts/sge/submit_dir_for_harvesting.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+### Import Vappio config
+vappio_scripts=/opt/vappio-scripts
+source $vappio_scripts/vappio_config.sh
+###
+
+vlog "###"
+vlog "### $0 (`whoami`) on `hostname`"
+vlog "###"
+
+# Exec host is passed in as $1
+exechost=$1
+dir=$2
+masterhost=`cat /mnt/clovr/runtime/master_node`
+
+# Harvest output
+vlog "Harvesting output from $exechost:$dir to $masterhost:$dir"
+vlog "CMD: $SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync y -q $harvestingq $vappio_scripts/harvesting.sh $exechost $dir"
+qsub_cmd="$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync y -q $harvestingq $vappio_scripts/harvesting.sh $exechost $dir"
+$qsub_cmd 1>> $vappio_log 2>> $vappio_log
+vlog "qsub return value: $?"
