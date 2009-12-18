6827e2801ca429c5c1432c08dd24534a664ce72d
Kevin Galens <kevingalens@fake.com>
2009-12-18 18:23:26 +0000

    syncdata now has an option to run staging synchronously (ie script will wait until stagin is complete before moving on)

--- vappio-scripts/syncdata.sh
+++ vappio-scripts/syncdata.sh
@@ -12,6 +12,12 @@
 vappio_scripts=/opt/vappio-scripts
 source $vappio_scripts/vappio_config.sh
 ##
+sync="n"
+if [ $1 ]; then
+    if [ "$1" == "--synchronous" ]; then
+        sync="y"
+    fi
+fi
 
 vlog "###" 
 vlog "### $0 (`whoami`) on `hostname`" 
@@ -49,7 +55,7 @@ for node in $stagingnodes
  do
 if [ "$node" != "$master" ]; then
 vlog "Reseeding $node in $stagingq"
-$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync n -q $stagingq $seeding_script $node $stagingq 1>> $vappio_log 2>> $vappio_log
+$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync $sync -q $stagingq $seeding_script $node $stagingq 1>> $vappio_log 2>> $vappio_log
 fi
 done
 
@@ -57,6 +63,6 @@ for node in $stagingsubnodes
  do
 if [ "$node" != "$master" ]; then 
 vlog "Reseeding $node in $stagingsubq"
-$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync n -q $stagingq,$stagingsubq $seeding_script $node $stagingsubq 1>> $vappio_log 2>> $vappio_log
+$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync $sync -q $stagingq,$stagingsubq $seeding_script $node $stagingsubq 1>> $vappio_log 2>> $vappio_log
 fi
 done
