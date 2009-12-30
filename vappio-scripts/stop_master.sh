e98224d709a5391930e218a4f658a6a38706877e
orbitz <orbitz@gmail.com>
2009-12-30 15:56:37 +0000

    Merging rc.local into local and making local a proper shutdown script

--- vappio-scripts/stop_master.sh
+++ vappio-scripts/stop_master.sh
@@ -16,27 +16,18 @@ echo "" > $SGE_ROOT/$SGE_CELL/common/act_qmaster
 myhostnameshort=`hostname`
 if [ -s $SGE_ROOT/default/spool/$myhostnameshort/execd.pid ]
 then
-        echo "qconf -kej appears to have failed. Checking $SGE_ROOT/default/spool/$myhostnameshort/execd.pid"
-        execpid=`cat $SGE_ROOT/default/spool/$myhostnameshort/execd.pid`
-        echo "Killing $execpid"
-        kill $execpid
+    echo "qconf -kej appears to have failed. Checking $SGE_ROOT/default/spool/$myhostnameshort/execd.pid"
+    execpid=`cat $SGE_ROOT/default/spool/$myhostnameshort/execd.pid`
+    echo "Killing $execpid"
+    kill $execpid
 fi
 
 # rm dirs in /mnt (otherwise they will be saved on Nimbus)
 for p in $harvesting_dir $staging_dir $wfworking_dir $scratch_dir /var/spool/sge /mnt/projects/clovr
- do
-rm -rf $p
- done
+  do
+  rm -rf $p
+done
 
-# remove Nimbus url if it exists
-rm /var/nimbus-metadata-server-url
-
-# nuke authorized_keys
-:> /root/.ssh/authorized_keys
-
-rm -rf /root/.svn
-
-rm -rf /root/.subversion
 
 echo "OFFLINE" > $vappio_runtime/node_type
 date > /root/last_stop_master
