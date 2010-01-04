33e1fd847b2544bc76f51ce5cac0d35c9f627aba
orbitz <orbitz@gmail.com>
2010-01-04 20:12:59 +0000

    Added crossbow to PATH

--- vappio-scripts/clovrEnv.sh
+++ vappio-scripts/clovrEnv.sh
@@ -6,5 +6,5 @@ source /opt/sge/default/common/settings.sh
 export PYTHONPATH=/opt/vappio-py
 export JAVA_HOME=/usr
   
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/opt/vappio-py/vappio/cli:/opt/vappio-scripts:/opt/vappio-scripts/pipelines
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/opt/vappio-py/vappio/cli:/opt/vappio-scripts:/opt/vappio-scripts/pipelines:/opt/crossbow/ec2-local:/opt/crossbow/local
 
