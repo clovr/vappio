0fd6b9266d36c4ea07d32df195f367c1d8b5b94c
Sam Angiuoli <angiuoli@fake.com>
2010-01-11 20:31:45 +0000

    reintroduced support for exec nodes to automatically shutdown if idle at the end of hour increments from startup

--- vappio-scripts/amazonec2/shutdownonidle.sh
+++ vappio-scripts/amazonec2/shutdownonidle.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-SGE_ROOT=/opt/sge
-SGE_QMASTER_PORT=6444
-SGE_EXECD_PORT=6445
-export SGE_ROOT
-export SGE_QMASTER_PORT
-export SGE_EXECD_PORT
+vappio_scripts=/opt/vappio-scripts
+source $vappio_scripts/vappio_config.sh
 
 #suggested improvement
 #poll over 10 minute interval, if no jobs, then proceed with shutdown, provide 5 minutes notice
 
-/opt/sge/bin/lx24-x86/qstat -u guest > /tmp/sge.running
+$SGE_ROOT/bin/$ARCH/qstat -u '*' > /tmp/sge.running
 
 if [ -s /tmp/sge.running ]
  then
