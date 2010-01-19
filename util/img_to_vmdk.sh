9910ed5a709ae33e7c5a8186ec2b5c34d6b0201d
Dave Riley <daveriley@fake.com>
2010-01-19 18:56:27 +0000

    Adding img_to_vmdk.sh to util which will convert a xen image to a vmware vmdk

---
+++ util/img_to_vmdk.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+#Raw disk image to VMWare conversion
+#Allows for conversion of Xen images to VMware
+#S. Angiuoli 12/09
+
+#This must all be performed as root or sudo-ed
+
+# img_to_vmdk.sh <path_to_xen_image> <path_to_grub_tarball> <path_to_output_vmdk>
+
+if [ $# -ne 3 ] 
+    then
+    echo "Usage: img_to_vmdk.sh <path_to_xen_image> <path_to_grub_tarball> <path_to_output_vmdk>"
+    exit 2
+fi
+
+#create a new blank raw disk image of desired size, eg 10G
+rm /mnt/shared/clovrVMware.raw
+qemu-img create -f raw /mnt/shared/clovrVMware.raw 10G
+
+#mount clovrVMware.raw as loopback device loop0
+losetup /dev/loop0 /mnt/shared/clovrVMware.raw
+
+#create a new partition with fdisk
+#scripted to create 1 large, bootable partition
+fdisk /dev/loop0 << .
+n
+p
+1
+
+
+a
+1
+w
+.
+
+#now we have a virtual hard drive aka /dev/loop0==/dev/hda
+#We need partition 1 like /dev/hda1==/dev/loop1
+
+fdisk -ul /dev/loop0
+
+#Take sector start and covert to bytes 63*512=32256
+#Now mount this partition as /dev/loop1
+
+losetup -o 32256 /dev/loop1 /dev/loop0
+
+#Now copy the image, a ready-to-go, already formatted ext3 partition into /dev/loop1
+#ie. dump release image clovr-vXbXrX.raw in loopback loop1
+
+dd if=$1 of=/dev/loop1
+
+#could stop at this point and format blank partition as ext3
+#mkfs.ext3 /dev/loop1
+#Then run grub install and can save the /dev/loop1 file (eg. vmdktemplate.raw)
+#This file can serve as a starting point for future conversions
+#dd if=/tmp/clovr-vXbXrX.raw of=vmdktemplate.raw seek=32256
+#This eliminates the need for root access to create mount point /dev/loop1
+#Once this is done skip to the qemu-img convert process
+
+#Ensure /boot includes a working kernel and grub files
+#I have a copy in /usr/local/projects/CloVR/images/grub-boot.tgz
+#This is the original boot directory from the Ubuntu 9.04 image from stacklet.com
+#which serves as the base for CloVR.
+mkdir /mnt/foo1
+mount /dev/loop1 /mnt/foo1
+cp grub-boot.tgz /mnt/foo1
+cd /mnt/foo1
+tar xvzf $2
+sync
+cd
+umount /mnt/foo1
+
+#Now finally install boot loader on MBR of clovrVMware.raw==loop0
+#sync
+
+grub --device-map=/dev/null << .
+device (hd0) /mnt/shared/clovrVMware.raw 
+root (hd0,0)
+setup (hd0)
+quit
+.
+#unmount
+losetup -d /dev/loop1
+losetup -d /dev/loop0
+
+#We now have a bootable image in raw disk format, clovrVMWare.raw
+
+#Proceed with the VMware conversion
+#Create vmdk
+qemu-img convert -f raw /mnt/shared/clovrVMware.raw -O vmdk $3
+
+#Simply update an existing .vmx file to reference this vmdk
+#And you are done 
+
+
