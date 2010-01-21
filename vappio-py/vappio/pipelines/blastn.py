606b624924d4183174e898abb56dc2c6c31c7336
orbitz <orbitz@gmail.com>
2010-01-21 20:00:34 +0000

    Need tags dir in there

--- vappio-py/vappio/pipelines/blastn.py
+++ vappio-py/vappio/pipelines/blastn.py
@@ -14,7 +14,7 @@ TEMPLATE_DIR = '/opt/clovr_pipelines/workflow/project_saved_templates/blastn_tmp
 
 OPTIONS = [
     ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.upload_dir}/tags/' + x, notNone)),
-    ('REF_DB_PATH', '', '--REF_DB_PATH', 'The reference db for the blast run', compose(tagToRefDBPath, lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('REF_DB_PATH', '', '--REF_DB_PATH', 'The reference db for the blast run', compose(tagToRefDBPath, lambda x : '${dirs.upload_dir}/tags/' + x, notNone)),
     ##
     # For SEQS_PER_FILE the function at the end may seem odd, but really this is just validating that they give us an int
     # we actually want it as a string in order to do the replacement in the config file, which is why we do
