139f06bb43724463d09355782499c9c60f8056d6
Cesar Arze <cesar.arze@gmail.com>
2010-01-25 17:10:09 +0000

    More fixes to submit_dir_for_harvesting to address rysncing to master node

--- vappio-scripts/sge/submit_dir_for_harvesting.sh
+++ vappio-scripts/sge/submit_dir_for_harvesting.sh
@@ -10,17 +10,17 @@ vlog "### $0 (`whoami`) on `hostname`"
 vlog "###"
 
 dir=$1
-master=`cat /mnt/clovr/runtime/master_node`
-exechost=`hostname`
+master=`cat $SGE_ROOT/$SGE_CELL/common/act_qmaster`
+exechost=`hostname -f`
 
-if [ "$exechost" != "$master"]
-then   
+if [ "$exechost" != "$master" ]; then   
     # Harvest output
     vlog "Harvesting output from $exechost:$dir to $master:$dir"
     vlog "CMD: $SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync y -q $harvestingq $vappio_scripts/harvesting.sh $exechost $dir"
     qsub_cmd="$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync y -q $harvestingq $vappio_scripts/harvesting.sh $exechost $dir"
     $qsub_cmd 1>> $vappio_log 2>> $vappio_log
     vlog "qsub return value: $?"
+
 else 
     vlog "Skipping harvesting from master node; master node does not need to harvest to itself."
 fi               
