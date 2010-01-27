e00a51abf27f84cc8ef7b9e6ff5b3efa61a030f4
orbitz <orbitz@gmail.com>
2010-01-27 04:35:09 +0000

    Adding better error support to pipelineStatus

--- vappio-www/py-www/pipelineStatus.py
+++ vappio-www/py-www/pipelineStatus.py
@@ -21,12 +21,13 @@ class PipelineStatus(CGIPage):
     def body(self):
         conf = configFromEnv()
         request = readQuery()
-        if request['pipelines']:
-            pipelines = [load(conf('env.VAPPIO_HOME'), p) for p in request['pipelines']]
-        else:
-            pipelines = loadAll(conf('env.VAPPIO_HOME'))
-            
+    
         try:
+            if request['pipelines']:
+                pipelines = [load(conf('env.VAPPIO_HOME'), p) for p in request['pipelines']]
+            else:
+                pipelines = loadAll(conf('env.VAPPIO_HOME'))
+        
             return json.dumps([True, dict([(p.name, getPipelineStatus(p)) for p in pipelines])])
         except Exception, err:
             return json.dumps([False, str(err)])
