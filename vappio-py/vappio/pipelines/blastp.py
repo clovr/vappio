2c66a9799622b63b231b94022e5428bd79c25e8b
orbitz <orbitz@gmail.com>
2010-01-27 19:03:27 +0000

    Moved all blasts to support the same base

--- vappio-py/vappio/pipelines/blastp.py
+++ vappio-py/vappio/pipelines/blastp.py
@@ -7,21 +7,10 @@ from igs.utils.cli import notNone, defaultIfNone, restrictValues
 
 from vappio.pipeline_tools.blast import tagToRefDBPath
 
+from vappio.ergatis.pipelines import blastn
+
 ##
 # Need to know where the template lives
 TEMPLATE_NAME = 'blastp_tmpl'
 
-
-OPTIONS = [
-    ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ('REF_DB_PATH', '', '--REF_DB_PATH', 'The reference db for the blast run', compose(tagToRefDBPath, lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ##
-    # For SEQS_PER_FILE the function at the end may seem odd, but really this is just validating that they give us an int
-    # we actually want it as a string in order to do the replacement in the config file, which is why we do
-    # str . int
-    ('SEQS_PER_FILE', '', '--SEQS_PER_FILE', 'Number of sequences per file, defaults to 1000', compose(str, int, defaultIfNone('1000'))),
-    ('EXPECT', '', '--EXPECT', 'e-value cutoff, default is 1e-5', defaultIfNone('1e-5')),
-    ('OTHER_OPTS', '', '--OTHER_OPTS', 'Other options to pass to blast', defaultIfNone('')),
-    ('FILTER', '', '--FILTER', 'Filter query, default is T', compose(restrictValues(['T', 'F']), defaultIfNone('T')))
-    ]
-
+OPTIONS = blastn.OPTIONS
