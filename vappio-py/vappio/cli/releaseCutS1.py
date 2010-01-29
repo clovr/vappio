54db3d30edd85eea0e1ad2b940f073c5d7c6c0fd
orbitz <orbitz@gmail.com>
2010-01-29 15:53:40 +0000

    First step in cutting a release

---
+++ vappio-py/vappio/cli/releaseCutS1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+##
+# Step 1 in cutting a release
+# This runs on the build box
+
+from igs.utils.commands import runSystemEx
+
+COMMANDS = [
+    """apt-get upgrade""",
+    """svn co https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/packages /opt/packages""",
+    """dpkg --get-selections > /opt/packages/packages.latest""",
+    """perl -e 'use ExtUtils::Installed;$inst = ExtUtils::Installed->new(); foreach my $mod ($inst->modules()){print "$mod ",$inst->version($mod),"\n"};' > /opt/packages/cpan.packages""",
+    """svn commit /opt/packages -m "Updating package directory for release" """,
+    """rm -rf /opt/packages""",
+    """updateAllDirs.py"""
+    ]
+
+def main():
+    for c in COMMANDS:
+        runSystemEx(c)
+
+
+if __name__ == '__main__':
+    main()
