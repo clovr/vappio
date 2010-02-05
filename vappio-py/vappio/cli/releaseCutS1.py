07dfdb7818add40c091b4dad8b004e5cd04f01a6
orbitz <orbitz@gmail.com>
2010-02-05 14:45:47 +0000

    Changed from upgrade to update

--- vappio-py/vappio/cli/releaseCutS1.py
+++ vappio-py/vappio/cli/releaseCutS1.py
@@ -6,7 +6,7 @@
 from igs.utils.commands import runSystemEx
 
 COMMANDS = [
-    """apt-get upgrade""",
+    """apt-get update""",
     """svn co https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/packages /opt/packages""",
     """dpkg --get-selections > /opt/packages/packages.latest""",
     """perl -e 'use ExtUtils::Installed;$inst = ExtUtils::Installed->new(); foreach my $mod ($inst->modules()){print "$mod ",$inst->version($mod),"\n"};' > /opt/packages/cpan.packages""",
