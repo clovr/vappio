06f0929fccafaa5bb17e48188e1a4da5feac40da
Sam Angiuoli <angiuoli@fake.com>
2010-02-18 22:02:54 +0000

    Changed behavior of --synchronous. It now spawns each staging job and waits on job completion.  This can improve performance by allowing multiple staging jobs to run in parallel.

--- vappio-scripts/syncdata.sh
+++ vappio-scripts/syncdata.sh
@@ -51,12 +51,20 @@ done
 #seeding.sh runs staging.sh and adds $node back to the proper queue
 #seeding will be run on data nodes that are already seeded and 
 #idle in stagingq and/or stagingsubq
+#Capture job id in the array $jobs, so we can check status
+i=0;
 
 for node in $stagingnodes
 do
     if [ "$node" != "$master" ]; then
 	vlog "Reseeding $node in $stagingq"
-	$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync $sync -q $stagingq $seeding_script $node $stagingq 1>> $vappio_log 2>> $vappio_log
+	jobid=`$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -q $stagingq $seeding_script $node $stagingq 2>> $vappio_log | perl -ne '($jobid) = ($_ =~ /Your job (\d+)/);print $jobid'`
+	if [ $jobid ]; then
+	    jobs[$i]=$jobid
+	    i=`expr $i + 1`
+	else
+	    vlog "Error submitting seeding job for node $node"
+	fi
     fi
 done
 
@@ -68,6 +76,34 @@ for node in `echo $stagingsubnodes $execnodes | sort -u`
 do
     if [ "$node" != "$master" ]; then 
 	vlog "Reseeding $node in $stagingsubq"
-	$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -sync $sync -q $stagingq,$stagingsubq $seeding_script $node $stagingsubq 1>> $vappio_log 2>> $vappio_log
+	jobid=`$SGE_ROOT/bin/$ARCH/qsub -o /mnt/scratch -e /mnt/scratch -S /bin/sh -b n -q $stagingq,$stagingsubq $seeding_script $node $stagingsubq 2>> $vappio_log | perl -ne '($jobid) = ($_ =~ /Your job (\d+)/);print $jobid'`
+	if [ $jobid ]; then
+	    jobs[$i]=$jobid
+	    i=`expr $i + 1`
+	else
+	    vlog "Error submitting seeding job for node $node"
+	fi
     fi
 done
+
+##
+#If synchronous, loop over all submitted jobs until they are done. Optionally, we could add a timeout here as well
+if [ "$sync" == "y" ]; then
+    vlog "sync=y. Waiting on job completion. Number of jobs submitted=${#jobs[*]}"
+    finishedjobs=0
+    #If all jobs are finished, then we are done
+    while [ $finishedjobs -ne ${#jobs[*]} ]
+      do
+      finishedjobs=0
+      for job in ${jobs[*]}
+	do
+	$SGE_ROOT/bin/$ARCH/qstat -j $job > /dev/null 2> /dev/null
+        #If return 0, job is still running. Otherwise, jobid is finished (or jobid is bad). Either way, we want to consider the job done
+	if [ $? -ne 0 ]; then
+	    finishedjobs=`expr $finishedjobs + 1`
+	fi
+	sleep 1
+      done
+      echo "Finished jobs=$finishedjobs. Number of jobs submitted=${#jobs[*]}"
+    done
+fi
