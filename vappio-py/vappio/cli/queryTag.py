66af94e7c092b6dbe1a0117f68811198e51ad925
orbitz <orbitz@gmail.com>
2010-03-03 22:10:29 +0000

    Adding queryTag support

---
+++ vappio-py/vappio/cli/queryTag.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from igs.utils.cli import buildConfigN, notNone, defaultIfNone
+
+from vappio.webservice.files import queryTag
+
+OPTIONS = [
+    ('host', '', '--host', 'Host of web services to connect to, defaults to local host', defaultIfNone('localhost')),
+    ('name', '', '--name', 'Name of cluster', notNone),
+    ('tag_name', '', '--tag-name', 'Name of tag', notNone),
+    ]
+
+def main(options, files):
+    queryTag(options('general.host'),
+             options('general.name'),
+             options('general.tag_name'))
+    
+
+if __name__ == '__main__':
+    main(*buildConfigN(OPTIONS))
+    
+
--- vappio-py/vappio/webservice/files.py
+++ vappio-py/vappio/webservice/files.py
@@ -5,6 +5,7 @@ from igs.cgi.request import performQuery
 
 TAGDATA_URL = '/vappio/tagData_ws.py'
 UPLOADTAG_URL = '/vappio/uploadTag_ws.py'
+QUERYTAG_URL = '/vappio/queryTag_ws.py'
 
 def tagData(host, name, tagName, files, recursive, expand, append, overwrite):
     performQuery(host, TAGDATA_URL, dict(name=name,
@@ -23,3 +24,6 @@ def uploadTag(host, name, tagName, srcCluster, dstCluster, expand):
                                            dst_cluster=dstCluster,
                                            expand=expand))
     
+
+def queryTag(host, name, tagName):
+    return performQuery(host, QUERYTAG_URL, dict(name=name, tag_name=tagName))
