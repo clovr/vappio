45d3d8e046bc66ed4c3e17af446e08a40db977bb
orbitz <orbitz@gmail.com>
2010-03-03 22:22:40 +0000

    need to convert config to dict

--- vappio-www/py-www/queryTag_ws.py
+++ vappio-www/py-www/queryTag_ws.py
@@ -6,6 +6,7 @@ import json
 
 from igs.cgi.handler import CGIPage, generatePage
 from igs.cgi.request import readQuery, performQueryNoParse
+from igs.utils.config import configToDict
 
 from vappio.tags.tagfile import loadTagFile
 
@@ -21,7 +22,7 @@ class QueryTag(CGIPage):
         if request['name'] == 'local':
             cluster = loadCluster('localhost', 'local')
             
-            return json.dumps([True, loadTagFile(os.path.join(cluster.config('dirs.tag_dir'), request['tag_name']))])
+            return json.dumps([True, configToDict(loadTagFile(os.path.join(cluster.config('dirs.tag_dir'), request['tag_name'])))])
         else:
             ##
             # Forward the request onto the appropriate machine
