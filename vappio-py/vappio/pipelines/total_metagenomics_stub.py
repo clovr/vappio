42b9c050003529b8d02db73ebde67753c2b9d4b4
Cesar Arze <cesar.arze@gmail.com>
2010-03-10 21:45:22 +0000

    Adding stub total_metagenomics pipeline without blastx and blastn steps

---
+++ vappio-py/vappio/pipelines/total_metagenomics_stub.py
@@ -0,0 +1,21 @@
+##
+# THIS SHOULD BE RUN ON THE REMOTE SIDE
+import os
+
+from igs.utils.functional import identity, compose
+from igs.utils.cli import notNone, defaultIfNone, restrictValues
+
+from vappio.pipeline_tools.blast import tagToRefDBPath
+
+##
+# Need to know where the template lives
+TEMPLATE_NAME = 'total_metagenomics_stub'
+
+
+OPTIONS = [
+    ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of SFF files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
+    ('DB_PATH', '', '--DB_PATH', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('SEQS_PER_FILE', '', '--SEQS_PER_FILE', 'Number of sequences per file produced by split_multifasta', defaultIfNone("1000")),
+    ('NUM_SEQS', '', '--NUM_SEQS', 'Number of sequences per bsml file produced by metagene', defaultIfNone("150")),
+    ('GROUP_COUNT', '', '--GROUP_COUNT', 'Group count to use in Ergatis', defaultIfNone("50"))
+    ]
