38afbbd80e3b248be8161a3dbacb7982ae46840d
Cesar Arze <cesar.arze@gmail.com>
2010-03-30 15:16:13 +0000

    total metagenomics pipeline: Fixed error in split_multifasta config

--- vappio-py/vappio/pipelines/total_metagenomics_stub.py
+++ vappio-py/vappio/pipelines/total_metagenomics_stub.py
@@ -15,7 +15,7 @@ TEMPLATE_NAME = 'total_metagenomics_stub'
 OPTIONS = [
     ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of SFF files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
     ('DB_PATH', '', '--DB_PATH', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
-    ('SEQS_PER_FILE', '', '--SEQS_PER_FILE', 'Number of sequences per file produced by split_multifasta', defaultIfNone("1000")),
+    ('SEQS_PER_FILE', '', '--SEQS_PER_FILE', 'Number of sequences per file produced by split_multifasta', defaultIfNone("")),
     ('CUTOFF', '', '--CUTOFF', 'Metagene gene calls below this nucleotide length are discarded', defaultIfNone("")),
     ('TOTAL_FILES','', '--TOTAL_FILES', 'Tell split_multifasta to produce exactly this amount of files', defaultIfNone("")),
     ('NUM_SEQS', '', '--NUM_SEQS', 'Number of sequences per bsml file produced by metagene', defaultIfNone("150")),
