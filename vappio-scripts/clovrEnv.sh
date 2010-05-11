f46ff93d6fdfa73d253977fa0bd6c3f3f80691ad
Cesar Arze <cesar.arze@gmail.com>
2010-05-11 18:32:00 +0000

    Updated environment to house variables needed by Qiime

--- vappio-scripts/clovrEnv.sh
+++ vappio-scripts/clovrEnv.sh
@@ -2,14 +2,18 @@
 
 # Get Sun Grid Engine (SGE) settings
 source /opt/sge/default/common/settings.sh
- 
+
+export BIOINF_HOME=/opt/opt-packages/bioinf-v1r4b1/
+export RDP_JAR_PATH=$BIOINF_HOME/rdp_classifier/rdp_classifier-2.0.jar
+
 export VAPPIO_HOME=/opt
-export PYTHONPATH=$PYTHONPATH:$VAPPIO_HOME/vappio-py
+export PYTHONPATH=$PYTHONPATH:$VAPPIO_HOME/vappio-py:$BIOINF_HOME/Denoiser/:$BIOINF_HOME/PyNAST/:$BIOINF_HOME/Qiime/lib/
 export JAVA_HOME=/usr
   
 export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/usr/sbin
 export PATH=$PATH:/opt/vappio-py/vappio/cli:/opt/vappio-py/vappio/cli/remote:/opt/vappio-scripts:/opt/vappio-scripts/pipelines
 export PATH=$PATH:/opt/crossbow/ec2-local:/opt/crossbow/local:/opt/samtools:/opt/cufflinks
+export PATH=$PATH:$BIOINF_HOME/cd-hit/:$BIOINF_HOME/FastTree:$BIOINF_HOME/rdp_classifier:$BIOINF_HOME/UCLUST/
 
 ##
 # Something weird for mongodb
