ff1abaf85f5d3b92c4a79e4a7da80d918644d7a7
orbitz <orbitz@gmail.com>
2010-06-04 15:02:59 +0000

    Adding hudson to updateAllDirs

--- vappio-py/vappio/cli/updateAllDirs.py
+++ vappio-py/vappio/cli/updateAllDirs.py
@@ -14,6 +14,7 @@ OPTIONS = [
     ('clovr_pipelines', '', '--clovr_pipelines', 'Update clovr_pipelines', defaultIfNone(False), True),
     ('vappio_py_www', '', '--vappio-py-www', 'Update vappio-www/py-ww', defaultIfNone(False), True),
     ('vappio_conf', '', '--vappio-conf', 'Update vappio/conf', defaultIfNone(False), True),
+    ('hudson', '', '--hudson', 'Update hudson', defaultIfNone(False), True),
     ('co', '', '--co', 'Check out rather than export', defaultIfNone(False), True),
     ]
 
@@ -50,20 +51,24 @@ def main(options, _args):
         runSystemEx("""chmod +x /opt/vappio-py/vappio/cli/*.py""")
     if options('general.vappio_scripts') or updateAll:
         grabFromSVN(options, 'https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/vappio-scripts', '/opt/vappio-scripts')
-        runSystemEx("""chmod -R +x /opt/vappio-scripts""")
-        runSystemEx("""cp -f /opt/vappio-scripts/clovrEnv.sh /root""")
-        runSystemEx("""cp -f /opt/vappio-scripts/local /etc/init.d/local""")
-        runSystemEx("""cp -f /opt/vappio-scripts/rc.local /etc/init.d/rc.local""")
-        runSystemEx("""cp -f /opt/vappio-scripts/screenrc /root/.screenrc""")
+        runSystemEx("""chmod -R +x /opt/vappio-scripts""", log=True)
+        runSystemEx("""cp -f /opt/vappio-scripts/clovrEnv.sh /root""", log=True)
+        runSystemEx("""cp -f /opt/vappio-scripts/local /etc/init.d/local""", log=True)
+        runSystemEx("""cp -f /opt/vappio-scripts/rc.local /etc/init.d/rc.local""", log=True)
+        runSystemEx("""cp -f /opt/vappio-scripts/screenrc /root/.screenrc""", log=True)
     if options('general.clovr_pipelines') or updateAll:
         grabFromSVN(options, 'https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/clovr_pipelines', '/opt/clovr_pipelines')
     if options('general.vappio_py_www') or updateAll:
         grabFromSVN(options, 'https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/vappio-www/py-www', '/var/www/vappio')
     ##
-    # Only wan to do this one when specified
+    # Only want to do this one when specified
     if options('general.vappio_conf'):
         grabFromSVN(options, 'https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/vappio-conf', '/opt/vappio-conf')
 
+    if options('general.hudson') or updateAll:
+        grabFromSVN(options, 'https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/hudson/hudson-config/jobs', '/var/lib/hudson/jobs')
+        grabFromSVN(options, 'https://clovr.svn.sourceforge.net/svnroot/clovr/trunk/hudson/hudson-scripts', '/opt/hudson')
+        runSystemEx("""chown -R hudson.nogroup /var/lib/hudson/jobs""", log=True)
 
 if __name__ == '__main__':
     main(*buildConfigN(OPTIONS))
