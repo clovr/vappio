22f08caeb29b60b2bf2410c200fdfc7832961ebd
orbitz <orbitz@gmail.com>
2010-06-08 13:24:19 +0000

    Added more error output when a request fails

--- vappio-py/igs/cgi/request.py
+++ vappio-py/igs/cgi/request.py
@@ -25,7 +25,7 @@ def performQuery(host, url, var, timeout=30, debug=False):
     try:
         ok, result = json.loads(data)
         if not ok:
-            raise TryError('Query failed', result)
+            raise TryError('Query failed: ' + result['msg'], result)
         return result
     except TryError:
         raise
