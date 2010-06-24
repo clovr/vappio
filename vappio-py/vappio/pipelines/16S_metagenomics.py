fcda01bb6bd6a336cf2902ee2829fb545e963c2f
James White <jamesrwhite@fake.com>
2010-06-24 20:10:16 +0000

    updated 16S pipeline inputs

--- vappio-py/vappio/pipelines/16S_metagenomics.py
+++ vappio-py/vappio/pipelines/16S_metagenomics.py
@@ -11,9 +11,12 @@ from vappio.pipeline_tools.blast import tagToRefDBPath
 # Need to know where the template lives
 TEMPLATE_NAME = '16S_metagenomics'
 
-
 OPTIONS = [
     ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of FASTA files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
     ('DB_PATH', '', '--DB_PATH', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('OLIGOS_FILE', '', '--OLIGOS_FILE', 'Oligos file', compose(lambda x : x, notNone)),
+    ('QUALITY_FILE', '', '--QUALITY_FILE', 'Quality file', compose(lambda x : x, notNone)),
+    ('META_FILE', '', '--META_FILE', 'Metadata file', compose(lambda x : x, notNone)),
     ('GROUP_COUNT', '', '--GROUP_COUNT', 'Ergatis group count', defaultIfNone("50"))
     ]
+
