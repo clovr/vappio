864583745e307fe8dc727988611d20e47436ba44
orbitz <orbitz@gmail.com>
2010-07-15 13:57:48 +0000

    Adding a bit of sleep between mounts

--- vappio-scripts/vmware/start_vmware.sh
+++ vappio-scripts/vmware/start_vmware.sh
@@ -21,11 +21,13 @@ fi
 
 # Generic Shared area
 mount -o ttl=3 -t vmhgfs .host:$shared_dir $shared_mp -o uid=33 -o gid=33
+sleep 1
 mount -o ttl=3 -t vmhgfs .host:$userdata_dir $userdata_mp -o uid=33 -o gid=33 -o fmask=000 -o dmask=000
+mount -o ttl=3 -t vmhgfs .host:/keys /mnt/keys -o uid=33 -o gid=33 -o fmask=077 -o dmask=077
 
 # Postgres specific shared area
 mount -o ttl=3 -t vmhgfs .host:$postgres_data_dir $postgres_data_dir_mp -o uid=$postgres_uid
-mount -o ttl=3 -t vmhgfs .host:/keys /mnt/keys -o uid=33 -o gid=33 -o fmask=077 -o dmask=077
+
 
 # Required permissions for postgres
 chmod 700 $postgres_data_dir_mp
