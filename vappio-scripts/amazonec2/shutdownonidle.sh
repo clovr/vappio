e9d5058fade7133fa562f4aa8725bdf1273bcb7e
Sam Angiuoli <angiuoli@fake.com>
2010-07-19 19:54:15 +0000

    added check for hadoop jobs. and override by placing file on master

--- vappio-scripts/amazonec2/shutdownonidle.sh
+++ vappio-scripts/amazonec2/shutdownonidle.sh
@@ -2,16 +2,27 @@
 
 #shutdownonidle.sh
 #Checks if a node is performing any work and if not schedules a shutdown in 5 minutes
+#Auto shutdown can be disabled by running placing a file on the master
+#touch /var/www/noautoshutdown
 
 vappio_scripts=/opt/vappio-scripts
 source $vappio_scripts/vappio_config.sh
 
 myhostname=`hostname -f`
+master=`cat $SGE_ROOT/$SGE_CELL/common/act_qmaster`
+/usr/bin/curl -f -s http://$master/noautoshutdown
+if [ $? == 0 ]
+then
+    vlog "SKIPPING AUTOSHUTDOWN. MANUAL OVERRIDE"
+    exit 0
+fi
 
 #Check for jobs running on this host
 $SGE_ROOT/bin/$ARCH/qstat -u '*' -l hostname=$myhostname > $vappio_runtime/sge.running 
 #Check for staging,harvesting jobs or other jobs submitted by this host
 $SGE_ROOT/bin/$ARCH/qstat | grep sge_o_host | grep $myhostname >> $vappio_runtime/sge.running 
+#Check for hadoop jobs
+/opt/hadoop/bin/hadoop job -list | grep running | /usr/bin/perl -ne '($num) = ($_ =~ /(\d+) jobs /);print $_ if($num>0);'>> $vappio_runtime/sge.running 
 
 if [ -s $vappio_runtime/sge.running ]
     then
@@ -28,6 +39,8 @@ do
     $SGE_ROOT/bin/$ARCH/qstat -u '*' -l hostname=$myhostname >> $vappio_runtime/sge.running 
     #Check for staging,harvesting jobs or other jobs submitted by this host
     $SGE_ROOT/bin/$ARCH/qstat | grep sge_o_host | grep $myhostname >> $vappio_runtime/sge.running 
+    #Check for hadoop jobs
+    /opt/hadoop/bin/hadoop job -list | grep running | /usr/bin/perl -ne '($num) = ($_ =~ /(\d+) jobs /);print $_ if($num>0);' >> $vappio_runtime/sge.running 
     #Shortcircuit if any running jobs
     if [ -s $vappio_runtime/sge.running ]
 	then
