d90acbb2cd35e37a619dd89037488033de1d3e3c
Sam Angiuoli <angiuoli@fake.com>
2010-07-23 14:52:07 +0000

    -t param missing to runPipeline so task name not printed

--- vappio-scripts/clovrEnv.sh
+++ vappio-scripts/clovrEnv.sh
@@ -46,7 +46,7 @@ ulimit -n 64000
 #export GLOBUS_LOCATION=/opt/opt-packages/globus-5.0.0
 #source $GLOBUS_LOCATION/etc/globus-user-env.sh
 
-clovrWrapper () { taskname=`runPipeline.py --name local --pipeline clovr_wrapper --pipeline-name clovr_wrapper$$ -- --CONFIG_FILE=$1` && taskStatus.py --block $taskname; }
+clovrWrapper () { taskname=`runPipeline.py --name local --pipeline clovr_wrapper --pipeline-name clovr_wrapper$$ -t -- --CONFIG_FILE=$1` && taskStatus.py --block $taskname; }
 clovrSleep () { clovrWrapper $1 ; }
 clovr16S () { clovrWrapper $1 ; }
 clovrMicrobe () { clovrWrapper $1 ; }
