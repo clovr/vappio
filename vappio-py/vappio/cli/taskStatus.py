65cce0712acfd3dc7e2b69812e4b16435e460a70
orbitz <orbitz@gmail.com>
2010-07-26 20:43:39 +0000

    taskStatus block now prints out the state of each task every 30 seconds and clears the screen.  Kind of useful instead of using watch

--- vappio-py/vappio/cli/taskStatus.py
+++ vappio-py/vappio/cli/taskStatus.py
@@ -8,6 +8,7 @@ from igs.utils.cli import buildConfigN, defaultIfNone
 from igs.utils.functional import identity
 from igs.utils.logging import debugPrint
 from igs.utils import logging
+from igs.utils import commands
 
 from vappio.webservice.task import loadAllTasks, loadTask
 
@@ -19,6 +20,7 @@ OPTIONS = [
     ('show_msgs', '', '--show', 'Print out any messages present', identity, True),
     ('show_error_msgs', '', '--show-error', 'Print out only error messages', identity, True),
     ('show_debug_msgs', '', '--show-debug', 'Print out any debug messages', identity, True),
+    ('show_all_msgs', '', '--show-all', 'Print out all messages', identity, True),    
     ('debug', '', '--debug', 'Print debugging messages', identity, True),
     ('no_completed', '', '--nc', 'Filtered out completed tasks', identity, True),
     ('exit_code', '', '--exit-code', 'Exit with a non zero value if any tasks are not in a completed state', identity, True),
@@ -29,16 +31,56 @@ OPTIONS = [
 def timestampToStr(ts):
     return time.strftime('%Y/%m/%d %H:%M:%S UTC', time.gmtime(ts))
 
+def showAnyMsg(options):
+    return (options('general.show_msgs') or
+            options('general.show_error_msgs') or
+            options('general.show_debug_msgs') or
+            options('general.show_all_msgs'))
+
+
+def printTask(options, t, maxTaskNameLen):
+        print 'Task: %s%s Type: %s%s State: %s%s Num: %d/%d (%3d%%) LastUpdated: %s' % (t.name, ' ' * (maxTaskNameLen - len(t.name)),
+                                                                                        t.tType, ' ' * (13 - len(t.tType)),
+                                                                                        t.state, ' ' * (13 - len(t.state)),
+                                                                                        t.completedTasks,
+                                                                                        t.numTasks,
+                                                                                        t.numTasks and int(float(t.completedTasks)/t.numTasks * 100.0) or 0,
+                                                                                        timestampToStr(t.timestamp))
+        if showAnyMsg(options):
+            for m in t.messages:
+                if m['mtype'] == task.MSG_NOTIFICATION and options('general.show_msgs') or options('general.show_all_msgs'):
+                    print 'Notification - %s: %s' % (timestampToStr(m['timestamp']), m['text'])
+                elif m['mtype'] == task.MSG_ERROR and options('general.show_error_msgs') or options('general.show_all_msgs'):
+                    print 'Error - %s: %s' % (timestampToStr(m['timestamp']), m['text'])
+                    if 'stacktrace' in m:
+                        indented = '\n\t'.join(m['stacktrace'].split('\n'))
+                        print 'Stacktrace:\n\t' + indented
+                elif m['mtype'] == task.MSG_SILENT and options('general.show_debug_msgs') or options('general.show_all_msgs'):
+                    print 'Debug - %s: %s' % (timestampToStr(m['timestamp']), m['text'])
+    
+
 
 def blockOnTasks(options, tasks):
+    maxTaskNameLen = tasks and max([len(t.name) for t in tasks]) or 0
     ##
     # Loop until all of the tasks are in state FAILED or COMPLETED
     while [t for t in tasks if t.state not in [task.TASK_FAILED, task.TASK_COMPLETED]]:
+        if showAnyMsg(options):
+            commands.runSystem('clear')
+            for t in tasks:
+                printTask(options, t, maxTaskNameLen)
         time.sleep(30)
         tasks = [loadTask(options('general.host'), options('general.name'), t.name)
                  for t in tasks]
 
+    ##
+    # If we are showing any messages then clear the screen because after this
+    # the tasks will be printed out again
+    if showAnyMsg(options):
+        commands.runSystem('clear')
+        
     return tasks
+
 
 
 def main(options, tasks):
@@ -73,24 +115,7 @@ def main(options, tasks):
             print
             print
 
-        print 'Task: %s%s Type: %s%s State: %s%s Num: %d/%d (%3d%%) LastUpdated: %s' % (t.name, ' ' * (maxTaskNameLen - len(t.name)),
-                                                                                        t.tType, ' ' * (13 - len(t.tType)),
-                                                                                        t.state, ' ' * (13 - len(t.state)),
-                                                                                        t.completedTasks,
-                                                                                        t.numTasks,
-                                                                                        t.numTasks and int(float(t.completedTasks)/t.numTasks * 100.0) or 0,
-                                                                                        timestampToStr(t.timestamp))
-        if options('general.show_msgs') or options('general.show_debug_msgs'):
-            for m in t.messages:
-                if m['mtype'] == task.MSG_NOTIFICATION and options('general.show_msgs'):
-                    print 'Notification - %s: %s' % (timestampToStr(m['timestamp']), m['text'])
-                elif m['mtype'] == task.MSG_ERROR and (options('general.show_msgs') or options('general.show_error_msgs')):
-                    print 'Error - %s: %s' % (timestampToStr(m['timestamp']), m['text'])
-                    if 'stacktrace' in m:
-                        indented = '\n\t'.join(m['stacktrace'].split('\n'))
-                        print 'Stacktrace:\n\t' + indented
-                elif m['mtype'] == task.MSG_SILENT and (options('general.show_msgs') or options('general.show_debug_msgs')):
-                    print 'Debug - %s: %s' % (timestampToStr(m['timestamp']), m['text'])
+        printTask(options, t, maxTaskNameLen)
 
     if options('general.exit_code'):
         debugPrint(lambda : 'Exiting with non-zero state if any tasks are not in a completed state')
