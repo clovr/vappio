640e95ce1b5aefa7801fd3c3b34e30e42eaac401
James White <jamesrwhite@fake.com>
2010-08-06 20:01:20 +0000

    added 16S_dual component to vappiopy

---
+++ vappio-py/vappio/pipelines/16S_dual.py
@@ -0,0 +1,19 @@
+##
+# THIS SHOULD BE RUN ON THE REMOTE SIDE
+import os
+
+from igs.utils.functional import identity, compose
+from igs.utils.cli import notNone, defaultIfNone, restrictValues
+
+from vappio.pipeline_tools.blast import tagToRefDBPath
+
+##
+# Need to know where the template lives
+TEMPLATE_NAME = '16S_dual'
+
+OPTIONS = [
+    ('FASTA_FILES', '', '--FASTA_FILES', 'The input file list of FASTA files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
+    ('MAPPING_FILE', '', '--MAPPING_FILE', 'Mapping data file', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
+    ('DB_PATH', '', '--DB_PATH', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('GROUP_COUNT', '', '--GROUP_COUNT', 'Ergatis group count', defaultIfNone("50"))
+    ]
