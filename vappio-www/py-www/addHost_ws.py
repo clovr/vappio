cbfc74adc2f6f2365f618dc83f44536cc652bf3e
Sam Angiuoli <angiuoli@fake.com>
2010-08-30 18:58:49 +0000

    fix for update to ubuntu 10.04 gridengine that moves sge location and SGE_ROOT

--- vappio-www/py-www/addHost_ws.py
+++ vappio-www/py-www/addHost_ws.py
@@ -10,7 +10,7 @@ from igs.cgi.handler import CGIPage, generatePage
 from igs.utils import commands
 
 
-SGE_ROOT = '/opt/sge'
+SGE_ROOTBIN = '/var/lib/gridengine/bin/lx26-ia64'
 
 
 class AddHost(CGIPage):
@@ -18,9 +18,7 @@ class AddHost(CGIPage):
         form = cgi.FieldStorage()
         host = form['host'].value
         sio = StringIO.StringIO()
-        commands.runSingleProgramEx(os.path.join(SGE_ROOT, 'util', 'arch'), stdoutf=sio.write, stderrf=None)
-        arch = sio.getvalue().strip()
-        sgeRootBin = os.path.join('/opt', 'sge', 'bin', arch)
+        sgeRootBin = os.path.join(SGE_ROOTBIN)
 
         if 'ipaddr' in form and re.match('^\d+\.\d+\.\d+\.\d+$', form['ipaddr'].value):
             commands.runSystemEx('addEtcHosts.py %s %s' % (host, form['ipaddr'].value))
