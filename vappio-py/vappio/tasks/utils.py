05885fb949337b00bb4f83261c993465226f01c0
orbitz <orbitz@gmail.com>
2010-09-02 16:03:10 +0000

    Wrong variable names

--- vappio-py/vappio/tasks/utils.py
+++ vappio-py/vappio/tasks/utils.py
@@ -97,4 +97,4 @@ def runTaskMain(func, options, args, optionsTaskName='general.task_name'):
     This is a little cheat function to wrap up running a task around what
     a standard main function looks like
     """
-    return runTask(o(optionsTaskName), lambda : func(o, a))
+    return runTask(options(optionsTaskName), lambda : func(options, args))
