f5cc8171de50ada4867ffef192eb3a8e7de4cc46
James White <jamesrwhite@fake.com>
2010-09-03 21:09:21 +0000

    new metagenomics pipelines

--- vappio-py/vappio/pipelines/clovr_metagenomics_noorf.py
+++ vappio-py/vappio/pipelines/clovr_metagenomics_noorf.py
@@ -15,7 +15,8 @@ TEMPLATE_NAME = 'clovr_metagenomics_noorf'
 OPTIONS = [
     ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of fasta files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
     ('MAPPING_FILE', '', '--MAPPING_FILE', 'The mapping file for all fastas', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)), 
-    ('DB_PATH', '', '--DB_PATH', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('NUCLEOTIDE_DB_PATH', '', '--NUCLEOTIDE_DB_PATH', 'The root nucelotide database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('PROTEIN_DB_PATH', '', '--PROTEIN_DB_PATH', 'The root protein database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
     ('SEQS_PER_FILE', '', '--SEQS_PER_FILE', 'Number of sequences per file produced by split_multifasta', defaultIfNone("")),
     ('CUTOFF', '', '--CUTOFF', 'Metagene gene calls below this nucleotide length are discarded', defaultIfNone("")),
     ('TOTAL_FILES','', '--TOTAL_FILES', 'Tell split_multifasta to produce exactly this amount of files', defaultIfNone("")),
