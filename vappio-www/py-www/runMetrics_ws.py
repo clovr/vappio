e5c26df5ac062cf1be16a8591969bcb3a1bd80dd
orbitz <orbitz@gmail.com>
2010-09-03 17:57:33 +0000

    adding run metrics web services

---
+++ vappio-www/py-www/runMetrics_ws.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from igs.cgi.handler import CGIPage, generatePage
+from igs.cgi.request import readQuery, performQuery
+from igs.utils.commands import runSystemEx
+
+from vappio.cluster import control as cluster_control
+
+from vappio.tasks.utils import createTaskAndSave
+
+
+class RunMetrics(CGIPage):
+    """
+    Metrics requires 3 options:
+    name - Name of cluster to run on
+    conf - Dictionary of config values
+    metrics - A string for what metrics to run.  Metrics are separated by the pipe symbol
+    """
+    def body(self):
+        request = readQuery()
+
+        if request['name'] == 'local':
+            taskName = createTaskAndSave('runMetric', 1, 'Running ' + request['metrics'])
+
+            cmd = ['runMetricsR.py',
+                   '--task-name=' + taskName,
+                   '--conf=' + json.dumps(request['conf']).encode('string_escape'),
+                   '--metrics=' + request['metrics'].encode('string_escape'),
+                   '>> /tmp/runMetricsR.py 2>&1 &',
+                   ]
+
+            runSystemEx(' '.join(cmd))
+        else:
+            #
+            # Forward request on
+            cluster = cluster_control.loadCluster(request['name'])
+            request['name'] = 'local'
+            taskName = performQuery(cluster.master.publicDNS, URL, request)
+
+        return taskName
+               
+
+        
+generatePage(RunMetrics())
