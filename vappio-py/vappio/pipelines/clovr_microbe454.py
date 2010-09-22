d8f1b56390964431aea4918002bd3de55895467a
orbitz <orbitz@gmail.com>
2010-09-22 18:51:00 +0000

    Updating microbe454 to new style

--- vappio-py/vappio/pipelines/clovr_microbe454.py
+++ vappio-py/vappio/pipelines/clovr_microbe454.py
@@ -10,16 +10,16 @@ from igs.utils.cli import notNone, defaultIfNone, restrictValues
 TEMPLATE_NAME = 'clovr_microbe454'
 
 OPTIONS = [
-    ('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ('OUTPUT_PREFIX', '', '--OUTPUT_PREFIX', 'Used in ID generation, Locus Tags, etc.', notNone),
-    ('CLEAR', '', '--CLEAR', 'Can be either all, 454, none, n, pair-of-n, or discard-n', compose(restrictValues(['all', '454', 'none', 'n', 'pair-of-n', 'discard-n']), defaultIfNone('discard-n'))),
-    ('TRIM', '', '--TRIM', 'Can be either none, soft, hard, chop', compose(restrictValues(['none', 'soft', 'hard', 'chop']), defaultIfNone('none'))),
-    ('LINKER', '', '--LINKER', 'If the 454 run was paired end, the linker type used (either titanium or flx. See sffToCA documentation for specific linker sequences for these two defaults). This could also be the sequence of the linker itself.', defaultIfNone('')),
-    ('INSERT_SIZE', '', '--INSERT_SIZE', 'Only needed if --LINKER option is specified. Mates are on average i +- d bp apart (i.e. --INSERT_SIZE "8000 1000").', defaultIfNone('')),
-    ('SPEC_FILE', '', '--SPEC_FILE', 'Spec file for celera assembler run', defaultIfNone('/dev/null')),
-    ('ORGANISM', '', '--ORGANISM', 'Organism name', defaultIfNone('/dev/null')),
-    ('GROUP_COUNT', '', '--GROUP_COUNT', 'Corresponds to number of groups to split data into (Ergatis)', defaultIfNone('50')),
-    ('DATABASE_PATH', '', '--DATABASE_PATH', 'The tag for the uploaded reference database set', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
-    ('SKIP_BANK', '', '--SKIP_BANK', 'Skip conversion of celera assembler output to AMOS bank format.', compose(lambda x : x and "toAmos,bank-transact,cleanup afg file" or "null", defaultIfNone(False)))
+    ('input.INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.INPUT_SFF_TAG}'))),
+    ('input.OUTPUT_PREFIX', '', '--OUTPUT_PREFIX', 'Used in ID generation, Locus Tags, etc.', notNone),
+    ('input.CLEAR', '', '--CLEAR', 'Can be either all, 454, none, n, pair-of-n, or discard-n', compose(restrictValues(['all', '454', 'none', 'n', 'pair-of-n', 'discard-n']), defaultIfNone('discard-n'))),
+    ('input.TRIM', '', '--TRIM', 'Can be either none, soft, hard, chop', compose(restrictValues(['none', 'soft', 'hard', 'chop']), defaultIfNone('none'))),
+    ('input.LINKER', '', '--LINKER', 'If the 454 run was paired end, the linker type used (either titanium or flx. See sffToCA documentation for specific linker sequences for these two defaults). This could also be the sequence of the linker itself.', defaultIfNone('')),
+    ('input.INSERT_SIZE', '', '--INSERT_SIZE', 'Only needed if --LINKER option is specified. Mates are on average i +- d bp apart (i.e. --INSERT_SIZE "8000 1000").', defaultIfNone('')),
+    ('input.SPEC_FILE', '', '--SPEC_FILE', 'Spec file for celera assembler run', defaultIfNone('/dev/null')),
+    ('input.ORGANISM', '', '--ORGANISM', 'Organism name', defaultIfNone('/dev/null')),
+    ('input.GROUP_COUNT', '', '--GROUP_COUNT', 'Corresponds to number of groups to split data into (Ergatis)', defaultIfNone('50')),
+    ('input.DATABASE_PATH', '', '--DATABASE_PATH', 'The tag for the uploaded reference database set', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.REFERENCE_DB_TAG}'))),
+    ('input.SKIP_BANK', '', '--SKIP_BANK', 'Skip conversion of celera assembler output to AMOS bank format.', compose(lambda x : x and "toAmos,bank-transact,cleanup afg file" or "null", defaultIfNone(False)))
     ]
 
