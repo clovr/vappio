006848860364e415282b5559abbeafaa43fa3a42
Mahesh Vangala <vangalamaheshh@gmail.com>
2010-09-28 20:14:09 +0000

    modified the clovr_pangenome.py

--- vappio-py/vappio/pipelines/clovr_pangenome.py
+++ vappio-py/vappio/pipelines/clovr_pangenome.py
@@ -9,6 +9,5 @@ from igs.utils.cli import notNone, defaultIfNone, restrictValues
 from vappio.pipeline_tools.blast import tagToRefDBPath
 TEMPLATE_NAME = 'clovr_pangenome'
 
-OPTIONS = [('INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-	   ('ORGANISM_TO_DB_MAPPING', '', '--ORGANISM_TO_DB_MAPPING', 'The input file to map the organism', compose(lambda x : '${dirs.tag_dir}/' + x, notNone))]
-
+OPTIONS = [('input.GENBANK_TAG', '', '--GENBANK_TAG', 'The input file list of GENBANK files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
+           ('input.MAP_TAG', '', '--MAP_TAG', 'The input file list of MAPPING files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone))]
