b6d94d9292a03042c442e69f53479f6b0a41785a
orbitz <orbitz@gmail.com>
2010-09-29 18:41:58 +0000

    Modified the python so it doesn't modify tags values

--- vappio-py/vappio/pipelines/clovr_16S.py
+++ vappio-py/vappio/pipelines/clovr_16S.py
@@ -12,8 +12,8 @@ from vappio.pipeline_tools.blast import tagToRefDBPath
 TEMPLATE_NAME = 'clovr_16S'
 
 OPTIONS = [
-    ('input.FASTA_TAG', '', '--FASTA_TAG', 'The input file list of FASTA files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ('input.MAPPING_TAG', '', '--MAPPING_TAG', 'Mapping data file', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ('input.DB_TAG', '', '--DB_TAG', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('input.FASTA_FILE_LIST', '', '--FASTA_TAG', 'The input file list of FASTA files', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.FASTA_TAG}'))),
+    ('input.MAPPING_FILE_LIST', '', '--MAPPING_TAG', 'Mapping data file', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.MAPPING_TAG}'))),
+    ('input.REF_DB_FILE_LIST', '', '--DB_TAG', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.REF_DB_TAG}'))),
     ('params.GROUP_COUNT', '', '--GROUP_COUNT', 'Ergatis group count', defaultIfNone("50"))
     ]
