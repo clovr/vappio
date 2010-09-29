ea5f6eed4a76a022a295c7eb1ffeb76e9c24fe80
orbitz <orbitz@gmail.com>
2010-09-29 15:38:18 +0000

    Updated clovr_sleep to new style

--- vappio-py/vappio/pipelines/clovr_sleep.py
+++ vappio-py/vappio/pipelines/clovr_sleep.py
@@ -1,24 +1,11 @@
 ##
 # THIS SHOULD BE RUN ON THE REMOTE SIDE
-import os
-
-from igs.utils.functional import identity, compose
-from igs.utils.cli import notNone, defaultIfNone, restrictValues
-
-from vappio.pipeline_tools.blast import tagToRefDBPath
-
-##
+#
 # Need to know where the template lives
 TEMPLATE_NAME = 'clovr_sleep'
 
-def fullPathOrTag(x):
-    if x[0] == '/':
-        return x
-    else:
-        return tagToRefDBPath(x)
-
 OPTIONS = [
-    ('TIME', '', '--TIME', 'Time to sleep', defaultIfNone('1')),
-    ('INPUT_FILE', '', '--INPUT_FILE', 'One or more input files', defaultIfNone('/mnt/staging'))
+    # ensure it is an int
+    ('input.TIME', '', '--TIME', 'Time to sleep', int),
     ]
 
