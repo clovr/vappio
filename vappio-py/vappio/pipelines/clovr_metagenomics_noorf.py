caedd68ee4b6313e1832ec40568457bc6203a720
James White <jamesrwhite@fake.com>
2010-10-01 19:49:12 +0000

    updated json metagenomics python scripts

--- vappio-py/vappio/pipelines/clovr_metagenomics_noorf.py
+++ vappio-py/vappio/pipelines/clovr_metagenomics_noorf.py
@@ -13,10 +13,10 @@ TEMPLATE_NAME = 'clovr_metagenomics_noorf'
 
 
 OPTIONS = [
-    ('input.FASTA_TAG', '', '--FASTA_TAG', 'The input file list of fasta files', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ('input.MAPPING_TAG', '', '--MAPPING_TAG', 'The mapping file for all fastas', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)), 
-    ('input.NUCLEOTIDE_DB_PATH', '', '--NUCLEOTIDE_DB_PATH', 'The root nucelotide database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
-    ('input.PROTEIN_DB_PATH', '', '--PROTEIN_DB_PATH', 'The root protein database path', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('input.FASTA_FILE_LIST', '', '--FASTA_TAG', 'The input file list of FASTA files', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.FASTA_TAG}'))),
+    ('input.MAPPING_FILE_LIST', '', '--MAPPING_TAG', 'Mapping data file', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.MAPPING_TAG}'))),
+    ('input.NUCLEOTIDE_DB_FILE_LIST', '', '--NUCLEOTIDE_DB_TAG', 'The root nucelotide database path', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.NUCLEOTIDE_DB_TAG}'))),
+    ('input.PROTEIN_DB_FILE_LIST', '', '--PROTEIN_DB_TAG', 'The root protein database path', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.PROTEIN_DB_TAG}'))),
     ('params.TOTAL_FILES','', '--TOTAL_FILES', 'Tell split_multifasta to produce exactly this amount of files', defaultIfNone("")),
     ('params.GROUP_COUNT', '', '--GROUP_COUNT', 'Group count to use in Ergatis', defaultIfNone("50"))
     ]
