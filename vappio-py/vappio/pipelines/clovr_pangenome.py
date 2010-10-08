67002399e265be964882adf057cd89daa7e2b693
Mahesh Vangala <vangalamaheshh@gmail.com>
2010-10-08 14:42:59 +0000

    fixed some changes needed for createpangenomemapfile component

--- vappio-py/vappio/pipelines/clovr_pangenome.py
+++ vappio-py/vappio/pipelines/clovr_pangenome.py
@@ -9,4 +9,4 @@ from igs.utils.cli import notNone, defaultIfNone, restrictValues
 from vappio.pipeline_tools.blast import tagToRefDBPath
 TEMPLATE_NAME = 'clovr_pangenome'
 
-OPTIONS = [('input.INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of GENBANK files', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.GENBANK_TAG}'))), ('input.ORGANISM_TO_DB_MAPPING', '', '--ORGANISM_TO_DB_MAPPING', 'The input file list of MAPPING files', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.MAP_TAG}')))]
+OPTIONS = [('input.INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of GENBANK files', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.GENBANK_TAG}')))]
