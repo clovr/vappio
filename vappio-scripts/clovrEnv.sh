a5a0f73e74a5d7b7149fab7bc0350eb98aa19d29
orbitz <orbitz@gmail.com>
2010-10-20 01:18:26 +0000

    Cleaning up clovrEnv a bit

--- vappio-scripts/clovrEnv.sh
+++ vappio-scripts/clovrEnv.sh
@@ -9,13 +9,13 @@ export JAVA_HOME=/usr/lib/jvm/java-6-openjdk/
   
 export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/usr/sbin
 export PATH=$PATH:/opt/vappio-py/vappio/cli:/opt/vappio-py/vappio/cli/remote:/opt/vappio-scripts:/opt/vappio-scripts/pipelines
-export PATH=$PATH:/opt/crossbow/ec2-local:/opt/crossbow/local:/opt/samtools:/opt/cufflinks:/opt/opt-packages/bowtie-0.12.0
+export PATH=$PATH:/opt/crossbow/ec2-local:/opt/crossbow/local:/opt/samtools:/opt/cufflinks:/opt/bowtie
 export PATH=$PATH:/opt/hadoop/bin
 export PATH=$PATH:$BIOINF_HOME/cd-hit/:$BIOINF_HOME/FastTree:$BIOINF_HOME/rdp_classifier:$BIOINF_HOME/UCLUST/:$BIOINF_HOME/MUSCLE/:$BIOINF_HOME/mafft/bin/:$BIOINF_HOME/PyNAST/bin/:$BIOINF_HOME/qiime/bin/:$BIOINF_HOME/mothur/:$BIOINF_HOME/microbiomeutil/ChimeraSlayer/
 
 ##
 # mugsy install
-export MUGSY_INSTALL=/opt/opt-packages/mugsy_x86-64-v1r1/
+export MUGSY_INSTALL=/opt/mugsy_x86-64
 export PATH=$PATH:$MUGSY_INSTALL
 
 ##
@@ -30,16 +30,16 @@ export QIIME_CONFIG_FP=$BIOINF_HOME/qiime/.qiime_config
 export BLASTMAT=/usr/share/ncbi/data/
 
 ## Crossbow environmental variables
-export CROSSBOW_HOME=/opt/opt-packages/crossbow-0.1.7
+export CROSSBOW_HOME=/opt/crossbow
 
 ##
 # These are temporary right now, trying to figure out the best way to 
 # allow a cluster to start up its own children
-export EC2_HOME=/opt/opt-packages/ec2-api-tools-1.3-53907
+export EC2_HOME=/opt/ec2-api-tools-1.3
 
 ##
 # EC2 api stuff needs to go in begining of path to override what is already on the VM
-export PATH=/opt/opt-packages/ec2-api-tools-1.3-53907/bin:$PATH
+export PATH=$EC2_HOME/bin:$PATH
 
 ##
 # These environmental varibales are required for hadoop to run without any errors
