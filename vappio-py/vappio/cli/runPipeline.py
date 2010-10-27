578bc19171293f8425b2a58f87d11f185a6c0596
orbitz <orbitz@gmail.com>
2010-10-27 20:04:27 +0000

    Adding code to add PIPELINE_WRAPPER_NAME and deprecating passing args to runPipeline

--- vappio-py/vappio/cli/runPipeline.py
+++ vappio-py/vappio/cli/runPipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ##
 # Runs a pipeline throuhg the webservice call
-from igs.utils.cli import buildConfigN, defaultIfNone, notNone
+from igs.utils import cli
 from igs.utils import config
 from igs.utils import functional as func
 
@@ -10,13 +10,13 @@ from vappio.webservice import pipeline
 from vappio.tasks.utils import runTaskStatus
 
 OPTIONS = [
-    ('host', '', '--host', 'Host of webservice to contact, defaults to localhost', defaultIfNone('localhost')),
-    ('name', '', '--name', 'Name of cluster', defaultIfNone('local')),
-    ('pipeline', '-p', '--pipeline', 'Type of pipeline', notNone),
-    ('pipeline_name', '-n', '--pipeline-name', 'Name to give the pipeline', notNone),
+    ('host', '', '--host', 'Host of webservice to contact, defaults to localhost', cli.defaultIfNone('localhost')),
+    ('name', '', '--name', 'Name of cluster', cli.defaultIfNone('local')),
+    ('pipeline', '-p', '--pipeline', 'Type of pipeline', cli.notNone),
+    ('pipeline_name', '-n', '--pipeline-name', 'Name to give the pipeline', cli.notNone),
     ('pipeline_config', '', '--pipeline-config', 'Config file to use for the pipeline', func.identity),
     ('pipeline_queue', '-q', '--pipeline-queue', 'Queue to use, not required', func.identity),
-    ('print_task_name', '-t', '--print-task-name', 'Print the name of the task at the end', defaultIfNone(False), True),
+    ('print_task_name', '-t', '--print-task-name', 'Print the name of the task at the end', cli.defaultIfNone(False), cli.BINARY),
     ]
 
 URL = '/vappio/runPipeline_ws.py'
@@ -26,18 +26,22 @@ def main(options, args):
         raise Exception('--pipeline-config and arguments are mutually exclusive')
     
     if args:
-        taskName = pipeline.runPipeline(options('general.host'),
-                                        options('general.name'),
-                                        options('general.pipeline'),
-                                        options('general.pipeline_name'),
-                                        args,
-                                        options('general.pipeline_queue'))
+        # taskName = pipeline.runPipeline(options('general.host'),
+        #                                 options('general.name'),
+        #                                 options('general.pipeline'),
+        #                                 options('general.pipeline_name'),
+        #                                 args,
+        #                                 options('general.pipeline_queue'))
+        raise Exception('No longer supporting passing arguments, all must be done through config files')
     else:
+        conf = config.configFromStream(open(options('general.pipeline_config')), lazy=True)
+        if options('general.pipeline') == 'clovr_wrapper':
+            conf = config.configFromMap({'pipeline.PIPELINE_WRAPPER_NAME': options('general.pipeline_name')}, base=conf, lazy=True)
         taskName = pipeline.runPipelineConfig(options('general.host'),
                                               options('general.name'),
                                               options('general.pipeline'),
                                               options('general.pipeline_name'),
-                                              config.configFromStream(open(options('general.pipeline_config')), lazy=True),
+                                              conf,
                                               options('general.pipeline_queue'))
 
 
@@ -48,4 +52,4 @@ def main(options, args):
     
         
 if __name__ == '__main__':
-    main(*buildConfigN(OPTIONS, usage='usage: %prog --name x --pipeline y [ -- options for pipeline]'))
+    main(*cli.buildConfigN(OPTIONS, usage='usage: %prog --name x --pipeline y [ -- options for pipeline]'))
