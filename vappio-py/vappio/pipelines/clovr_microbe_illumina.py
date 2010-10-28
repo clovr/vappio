fa0d8364d260a992742fa583e3a4c8779c374a05
Kevin Galens <kevingalens@fake.com>
2010-10-28 18:38:25 +0000

    added composeCLI instead of compose

--- vappio-py/vappio/pipelines/clovr_microbe_illumina.py
+++ vappio-py/vappio/pipelines/clovr_microbe_illumina.py
@@ -4,7 +4,7 @@ import os
 import re
 
 from igs.utils.functional import identity, compose
-from igs.utils.cli import notNone, defaultIfNone, restrictValues
+from igs.utils.cli import notNone, defaultIfNone, restrictValues, composeCLI
 
 ##
 # Need to know where the template lives
@@ -12,10 +12,10 @@ TEMPLATE_NAME = 'clovr_microbe_illumina'
 
 
 OPTIONS = [
-    ('input.SHORT_PAIRED_LIST', '', '--SHORT_PAIRED_LIST', 'A comma separated list of input tags. Each tag should contain two files.', compose( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x and re.split('[\s,]+', x) or []]), defaultIfNone('${input.SHORT_PAIRED_FILES}'))),
-    ('input.LONG_PAIRED_LIST', '', '--LONG_PAIRED_LIST', 'A common separated list of input tags. Each tag should contain two files', compose( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x and re.split('[\s,]+', x) or []]), defaultIfNone('${input.LONG_PAIRED_FILES}') ) ),
-    ('input.SHORT_INPUT_LIST', '', '--SHORT_INPUT_LIST', 'Input tag for non paired end short read data.', compose( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x.split()]), defaultIfNone('${input.SHORT_FILES}')) ),
-    ('input.LONG_INPUT_LIST', '', '--LONG_INPUT_LIST', 'Input tag for non paired long read data', compose( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x.split()]), defaultIfNone('${input.LONG_FILES}')) ),
+    ('input.SHORT_PAIRED_LIST', '', '--SHORT_PAIRED_LIST', 'A comma separated list of input tags. Each tag should contain two files.', composeCLI( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x and re.split('[\s,]+', x) or []]), defaultIfNone('${input.SHORT_PAIRED_FILES}'))),
+    ('input.LONG_PAIRED_LIST', '', '--LONG_PAIRED_LIST', 'A common separated list of input tags. Each tag should contain two files', composeCLI( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x and re.split('[\s,]+', x) or []]), defaultIfNone('${input.LONG_PAIRED_FILES}') ) ),
+    ('input.SHORT_INPUT_LIST', '', '--SHORT_INPUT_LIST', 'Input tag for non paired end short read data.', composeCLI( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x.split()]), defaultIfNone('${input.SHORT_FILES}')) ),
+    ('input.LONG_INPUT_LIST', '', '--LONG_INPUT_LIST', 'Input tag for non paired long read data', composeCLI( lambda x : ','.join(['${dirs.tag_dir}/' + y for y in x.split()]), defaultIfNone('${input.LONG_FILES}')) ),
     ('input.OUTPUT_PREFIX', '', '--OUTPUT_PREFIX', 'Used in ID generation, Locus Tags, etc.', notNone),
     ('input.START_HASH_LENGTH', '', '--START_HASH_LENGTH', 'Starting hash size: default 19. Must be Odd.', defaultIfNone('19')),
     ('input.END_HASH_LENGTH', '', '--END_HASH_LENGTH', 'Ending hash size: default 31. Must be Odd.', defaultIfNone('31')),
