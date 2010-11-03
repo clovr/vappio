1587d20a9d508eae038451da4e2a95228d1b2f71
Sam Angiuoli <angiuoli@fake.com>
2010-11-03 21:51:27 +0000

    fix for running microbe_annotation standalone

--- vappio-py/vappio/pipelines/clovr_microbe_annotation.py
+++ vappio-py/vappio/pipelines/clovr_microbe_annotation.py
@@ -10,10 +10,10 @@ from igs.utils.cli import notNone, defaultIfNone, restrictValues
 TEMPLATE_NAME = 'clovr_microbe_annotation'
 
 OPTIONS = [
-    ('INPUT_FSA_LIST', '', '--INPUT_FSA_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, notNone)),
-    ('OUTPUT_PREFIX', '', '--OUTPUT_PREFIX', 'Used in ID generation, Locus Tags, etc.', notNone),
-    ('ORGANISM', '', '--ORGANISM', 'Organism name', defaultIfNone('/dev/null')),
-    ('GROUP_COUNT', '', '--GROUP_COUNT', 'Corresponds to number of groups to split data into (Ergatis)', defaultIfNone('50')),
-    ('DATABASE_PATH', '', '--DATABASE_PATH', 'The tag for the uploaded reference database set', compose(lambda x : '${dirs.upload_dir}/' + x, notNone)),
+    ('input.INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.INPUT_FSA_TAG}'))),
+    ('input.OUTPUT_PREFIX', '', '--OUTPUT_PREFIX', 'Used in ID generation, Locus Tags, etc.', notNone),
+    ('input.ORGANISM', '', '--ORGANISM', 'Organism name', defaultIfNone('/dev/null')),
+    ('input.GROUP_COUNT', '', '--GROUP_COUNT', 'Corresponds to number of groups to split data into (Ergatis)', defaultIfNone('50')),
+    ('input.DATABASE_PATH', '', '--DATABASE_PATH', 'The tag for the uploaded reference database set', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.REFERENCE_DB_TAG}'))),
     ]
 
