eaae0f4fb2da97f270f13e5ef1ee01eea5a09d6b
orbitz <orbitz@gmail.com>
2010-11-03 18:14:32 +0000

    Adding script to make life easier when using diag

---
+++ vappio-scripts/diag/diag-env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Simple script to give you the diag env to run ec2 commands.
+# Takes 2 parameters, first is path to cert file, second is path to key file
+
+export EC2_CERT=$1
+export EC2_PRIVATE_KEY=$2
+
+export EC2_URL=https://diagcloud.igs.umaryland.edu:8443/wsrf/services/ElasticNimbusService
+
+export EC2_JVM_ARGS="-Djavax.net.ssl.trustStore=/tmp/jssecacerts"
+
+export EC2_HOME=/opt/ec2-api-tools-1.3-42584
+
+export PATH=$EC2_HOME/bin:$PATH
