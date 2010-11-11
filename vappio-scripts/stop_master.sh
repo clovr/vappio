545d1b091a52313b97a61c71529da426c7e23fd3
Sam Angiuoli <angiuoli@fake.com>
2010-11-11 23:46:59 +0000

    fixes for stopping master

--- vappio-scripts/stop_master.sh
+++ vappio-scripts/stop_master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #stop_master.sh
-#Stops a Vappio master node
+#Stops a vappio master node
 
 ##Import vappio config
 vappio_scripts=/opt/vappio-scripts
@@ -10,24 +10,27 @@ source $vappio_scripts/vappio_config.sh
 # Force remove all running jobs
 $SGE_ROOT/utilbin/$ARCH/qdel -f -u '*'
 
-#Should probably wait here for all jobs to finish
+#TODO, should probably wait here for all jobs to finish
+sleep 5
 
 # Remove all hosts and queues
 $vappio_scripts/sge/wipe_queues.sh
 
-/etc/init.d/gridengine-master stop
-
-echo "" > $SGE_ROOT/$SGE_CELL/common/act_qmaster
-
+#Stop execd
 #if -kej failed try again
 myhostnameshort=`hostname`
-if [ -s $SGE_ROOT/default/spool/$myhostnameshort/execd.pid ]
+/etc/init.d/gridengine-exec stop
+if [ -s /var/run/gridengine-execd.pid ]
 then
     echo "qconf -kej appears to have failed. Checking $SGE_ROOT/default/spool/$myhostnameshort/execd.pid"
     execpid=`cat $SGE_ROOT/default/spool/$myhostnameshort/execd.pid`
     echo "Killing $execpid"
     kill $execpid
 fi
+
+/etc/init.d/gridengine-master stop
+
+echo "" > $SGE_ROOT/$SGE_CELL/common/act_qmaster
 
 ##
 # Cleaning up some files specific to the master
