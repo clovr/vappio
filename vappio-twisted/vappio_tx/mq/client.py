9b82816529df6b323468b36edc50253eea915704
orbitz <orbitz@gmail.com>
2010-11-11 16:26:41 +0000

    Adding stomper and forking

--- vappio-twisted/vappio_tx/mq/client.py
+++ vappio-twisted/vappio_tx/mq/client.py
@@ -4,6 +4,39 @@
 # To use, call makeService with a config so it knows who to connect
 # and any username password login information.
 
+from zope import interface
+
+class IMQClientService(interface.Interface):
+    """
+    A message queue client
+    """
+
+    def subscribe(handler, destination, headers):
+        """
+        Subscribe to a queue or topic and us the list of headers given on it
+
+        A deferred is returned, if success None is passed, if failure, the failure
+
+        handler is a function that is called upon the incoming of a message
+        """
+
+    def unsubscribe(destination):
+        """
+        Unsubscribe form a destination
+        """
+
+    def send(destination, headers, body):
+        """
+        Send a message to the destination with the headers and the body.
+
+        A deferred is returned, if success None is passed, if failure, the failure
+        """
+
+    def ack(messageId, headers):
+        """
+        Acknolwedge a message
+        """
+
 
 
 
