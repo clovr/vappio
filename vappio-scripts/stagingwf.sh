490e4f534e62fa7eea55efe73dedd552a2121981
Sam Angiuoli <angiuoli@fake.com>
2010-11-11 23:41:31 +0000

    prelim support for staging data outside of workflow iterators

--- vappio-scripts/stagingwf.sh
+++ vappio-scripts/stagingwf.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #
 #USAGE: staging.wf.sh [remotehost] [workflow_xml]
-#The input workflow XML must the absolute path to a single group from an Ergatis iterator.
 #
-
-#This script parse the workflow XML and perform 2 copies
+#The input workflow XML must the absolute path to Ergatis workflow
+#XML, including a single group from an Ergatis iterator.
+#
+#
+#
+#This script parses the workflow XML and perform a number of data transfers
+#In the case of an Ergatis iterator
 #1)Copy the input files for the group to remotehost. The input files
 #  are referenced in a file name $groupnumber.iter
-
+#
 #2)Copy the workflow XML for the group to remotehost.  The workflow
 #  XML is contained in a directory g$groupnumber
+#
+#In all other cases, simply copy the workflow xml, argument 2 to this script
 
 ##Import vappio config
 vappio_scripts=/opt/vappio-scripts
@@ -32,9 +38,11 @@ vlog "wfcomponentdir: $wfcomponentdir"
 vlog "wfgroupdir: $wfgroupdir"
 vlog "group: $group"
 
+
+#Detect if this looks like a workflow iterator and transfer needed files; otherwise just transfer a wfxml
 #TODO transfer list of files as a single transfer instead of one at a time
 vlog "Start transfer of input from $wfgroupdir/$group.iter to $remotehost" 
-#Check for $;I_FILE_PATH$;
+#Check for $;I_FILE_PATH$;, if this exists good indicator we are in an iterator
 groupitertype=`cat $wfcomponentdir/$wfgroupdir/$group.iter | grep 'I_FILE_PATH'`
 if [ "$groupitertype" != "" ]
 then
@@ -58,18 +66,37 @@ then
 	fi
     done 
 else
-	vlog "No files found for staging in iterator $wfcomponentdir/$wfgroupdir/$group.iter. Skipping file staging"
+	vlog "No files found for staging in iterator $wfcomponentdir/$wfgroupdir/$group.iter. Skipping group file staging"
+	#Just stage workflow xml
+	vlog "Transfering $wfxml to $remotehost:$wfxml" 
+	vlog "CMD: rsync -av -R -O -e \"$ssh_client -i $ssh_key $ssh_options\" $wfxml root@$remotehost:/"
+	rsync -av -R -O -e "$ssh_client -i $ssh_key $ssh_options" $wfxml root@$remotehost:/ 1>> $vappio_log 2>> $vappio_log
+	if [ $? == 0 ]
+	then
+	    vlog "rsync success. return value: $?"
+	else
+	    vlog "ERROR: $0 rsync fail. return value: $?"
+	    verror "STAGING WF GROUP $wfcomponentdir/$wfgroupdir/$group.iter FAILURE"
+	    exit 1;
+	fi
 fi
-cd $wfcomponentdir
-vlog "Start transfer of workflow xml from $wfcomponentdir/$wfgroupdir to $remotehost:$wfcomponentdir" 
-vlog "CMD: rsync -av -R -e \"$ssh_client -i $ssh_key $ssh_options\" *.final.config $wfgroupdir root@$remotehost:$wfcomponentdir" 
-rsync -av -R -e "$ssh_client -i $ssh_key $ssh_options" *.final.config $wfgroupdir root@$remotehost:$wfcomponentdir 1>> $vappio_log 2>> $vappio_log
-if [ $? == 0 ]
+
+#Stage config file for a workflow iterator
+configfiles=`ls $wfcomponentdir/*.final.config`
+if [ "$configfiles" != "" ]
 then
-    vlog "rsync success. return value: $?"
-else
-    vlog "ERROR: $0 rsync fail. return value: $?"
-    verror "STAGING WF XML,CONFIG FAILURE"
-    exit 1;
+    cd $wfcomponentdir
+    vlog "Start transfer of workflow xml from $wfcomponentdir/$wfgroupdir to $remotehost:$wfcomponentdir" 
+    vlog "CMD: rsync -av -R -e \"$ssh_client -i $ssh_key $ssh_options\" *.final.config $wfgroupdir root@$remotehost:$wfcomponentdir" 
+    rsync -av -R -e "$ssh_client -i $ssh_key $ssh_options" *.final.config $wfgroupdir root@$remotehost:$wfcomponentdir 1>> $vappio_log 2>> $vappio_log
+    if [ $? == 0 ]
+    then
+	vlog "rsync success. return value: $?"
+    else
+	vlog "ERROR: $0 rsync fail. return value: $?"
+	verror "STAGING WF XML,CONFIG FAILURE"
+	exit 1;
+    fi
 fi
+
 
