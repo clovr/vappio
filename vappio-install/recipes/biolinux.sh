90c0618950ebee2ae5336baa2164b7aabbc2fe57
Sam Angiuoli <angiuoli@fake.com>
2010-11-15 22:51:38 +0000

    merging repos

--- vappio-install/recipes/biolinux.sh
+++ vappio-install/recipes/biolinux.sh
@@ -7,7 +7,6 @@ apt-get update
 apt-get --force-yes -y install bio-linux-keyring
 apt-get update
 
-#TODO, consider removing bio-linux-blast bioperl bio-linux-mummer bio-linux-hmmer
-#Replace tigr-glimmer bio-linux-glimmer3
-apt-get -y --force-yes install bio-linux-base-directories bio-linux-blast bio-linux-bldp-files bio-linux-glimmer3 bio-linux-hmmer bio-linux-mummer bio-linux-trnascan 
+#TODO, consider merging with Ubuntu repository, see recipe ubuntuifxpkgs
+apt-get -y --force-yes install bio-linux-base-directories bio-linux-bldp-files bio-linux-glimmer3 bio-linux-hmmer bio-linux-trnascan 
 
--- vappio-install/recipes/ubuntuifxpkgs.sh
+++ vappio-install/recipes/ubuntuifxpkgs.sh
@@ -2,5 +2,5 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y --force-yes install dialign hmmer mummer primer3 emboss emboss-data emboss-lib ncbi-data ncbi-tools-bin bioperl biosquid blast2 blt 
+apt-get -y --force-yes install dialign mummer primer3 emboss emboss-data emboss-lib ncbi-data ncbi-tools-bin bioperl biosquid blast2 blt 
 
