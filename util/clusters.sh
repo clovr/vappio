2d698e394cfefbd784cf8b7fb90f126eab222f95
orbitz <orbitz@gmail.com>
2010-11-29 15:46:09 +0000

    Modifying clusters.sh to parse new output of vmStatus

--- util/clusters.sh
+++ util/clusters.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
 
-clusters=`cat /tmp/vmStatus.log | perl -ne '/Clusters: (\w+)/;print $1'`
+clusters=`cat /tmp/vmStatus.log | perl -ne '/Clusters: ([^:]+)/;print $1'`
 
-echo -n "Clusters (# instances) : "
-for c in $clusters 
-do
-    if [ "$c" != "local" ]
-    then
-	num=`vp-describe-clusters --name=$c | grep -c EXEC`
-	echo -n "$c($num) ";
-    else
-	echo -n "$c ";
-    fi
-done  
+echo -n "Clusters (# instances) : $clusters"
 echo
-instances=`cat /tmp/vmStatus.log | perl -ne '/Instances: (\d+)/;print $1'`
-echo -n "Credentials            : "
-credentials=`vp-describe-credentials | perl -ne '/CRED\s+(\w+)/;print "$1 "'`
-for r in $credentials
-do
-    if [ "$r" != "local" ]
-    then
-	echo -n "$r($instances) "
-    else
-	echo -n "$r "
-    fi
-done
+
+credentials=`cat /tmp/vmStatus.log | perl -ne '/Credentials: ([^:]+)/;print $1'`
+echo -n "Credentials            : $credentials"
 echo 
