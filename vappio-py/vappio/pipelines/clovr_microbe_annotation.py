776bc264229a1fb0f6e382b24bc85ddb70dd159d
Kevin Galens <kevingalens@fake.com>
2011-01-21 14:35:49 +0000

    fixed section of GROUP_COUNT param

--- vappio-py/vappio/pipelines/clovr_microbe_annotation.py
+++ vappio-py/vappio/pipelines/clovr_microbe_annotation.py
@@ -13,7 +13,7 @@ OPTIONS = [
     ('input.INPUT_FILE_LIST', '', '--INPUT_FILE_LIST', 'The input file list of sequences', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.INPUT_FSA_TAG}'))),
     ('params.OUTPUT_PREFIX', '', '--OUTPUT_PREFIX', 'Used in ID generation, Locus Tags, etc.', notNone),
     ('params.ORGANISM', '', '--ORGANISM', 'Organism name', defaultIfNone('/dev/null')),
-    ('input.GROUP_COUNT', '', '--GROUP_COUNT', 'Corresponds to number of groups to split data into (Ergatis)', defaultIfNone('50')),
+    ('pipeline.GROUP_COUNT', '', '--GROUP_COUNT', 'Corresponds to number of groups to split data into (Ergatis)', defaultIfNone('50')),
     ('input.DATABASE_PATH', '', '--DATABASE_PATH', 'The tag for the uploaded reference database set', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.REFERENCE_DB_TAG}'))),
     ]
 
