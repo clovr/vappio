f9e3299d66247c5fef7a682bac2f797260d507f9
orbitz <orbitz@gmail.com>
2011-01-26 21:18:26 +0000

    Adding some junk code to retry loading tasks

--- vappio-py/vappio/tasks/persist.py
+++ vappio-py/vappio/tasks/persist.py
@@ -1,6 +1,7 @@
 ##
 # For persisting and loading tasks to the database
 # This uses MongoDB has backend
+import time
 
 import pymongo
 
@@ -10,11 +11,15 @@ class TaskDoesNotExistError(Exception):
     """A task does not exist in the db"""
     pass
 
-def load(taskName):
+def load(taskName, tries=3):
     task = pymongo.Connection().clovr.tasks.find_one(dict(name=taskName))
-    if task is None:
+    if task is None and tries < 0:
         raise TaskDoesNotExistError(taskName)
-    return task
+    elif task is None:
+        time.sleep(1)
+        return load(taskName, tries - 1)
+    else:
+        return task
 
 def dump(task):
     return pymongo.Connection().clovr.tasks.save(updateDict(dict(_id=task['name']), task))
