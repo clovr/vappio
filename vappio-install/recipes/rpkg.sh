aa1753cdaa430ada0a3e94a36003eefdfc9c6ff1
James White <jamesrwhite@fake.com>
2011-02-22 20:36:00 +0000

    updated R lib recipe

--- vappio-install/recipes/rpkg.sh
+++ vappio-install/recipes/rpkg.sh
@@ -4,6 +4,6 @@
 apt-get -y install r-base-core
 
 # Need to install gplots package and RColorBrewer pkg
-echo "install.packages(c(\"gplots\", \"RColorBrewer\") , repos=\"http://watson.nci.nih.gov/cran_mirror/\")" | R --save --restore
+echo "install.packages(c(\"gplots\", \"RColorBrewer\") , repos=\"http://www.ibiblio.org/pub/languages/R/CRAN\")" | R --save --restore
 
 # done
