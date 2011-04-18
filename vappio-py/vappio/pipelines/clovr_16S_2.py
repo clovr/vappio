452a7ff65f8c9c45f806d5d5f340c78fff1dd9cb
James White <jamesrwhite@fake.com>
2011-04-18 17:33:19 +0000

    no REFDBTAG in new 16S pipe

--- vappio-py/vappio/pipelines/clovr_16S_2.py
+++ vappio-py/vappio/pipelines/clovr_16S_2.py
@@ -14,6 +14,5 @@ TEMPLATE_NAME = 'clovr_16S_2'
 OPTIONS = [
     ('input.FASTA_FILE_LIST', '', '--FASTA_TAG', 'The input file list of FASTA files', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.FASTA_TAG}'))),
     ('input.MAPPING_FILE_LIST', '', '--MAPPING_TAG', 'Mapping data file', compose(lambda x : '${dirs.tag_dir}/' + x, defaultIfNone('${input.MAPPING_TAG}'))),
-    ('input.REF_DB_FILE_LIST', '', '--DB_TAG', 'The root database path', compose(lambda x : '${dirs.upload_dir}/' + x, defaultIfNone('${input.REF_DB_TAG}'))),
     ('params.GROUP_COUNT', '', '--GROUP_COUNT', 'Ergatis group count', defaultIfNone("50"))
     ]
