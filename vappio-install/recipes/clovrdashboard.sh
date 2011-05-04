e08bd69dfe06eb1c93245b2caef771504fcf300f
Dave Riley <daveriley@fake.com>
2011-05-04 15:06:16 +0000

    Added a mv to the dashboard recipe to move any existing index out of the way.

--- vappio-install/recipes/clovrdashboard.sh
+++ vappio-install/recipes/clovrdashboard.sh
@@ -4,5 +4,8 @@ export VAPPIO_HOME=/opt
 export PYTHONPATH=$PYTHONPATH:$VAPPIO_HOME/vappio-py
 source /opt/vappio-scripts/clovrEnv.sh
 updateAllDirs.py --clovr-www
-cp -f /var/www/clovr/redirect.html /var/www/index.html
+
+# Put the redirect html in place
+mv -f /var/www/index.html /var/www/index.html_preclovr
+cp /var/www/clovr/redirect.html /var/www/index.html
 /etc/init.d/apache2 reload
