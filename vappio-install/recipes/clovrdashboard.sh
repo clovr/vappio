e3a9da556f4bf1bb50240637f78484b3d38f2379
Dave Riley <daveriley@fake.com>
2011-05-04 15:01:13 +0000

    clovrdashboard now copies the redirect html from clovr-www to the www root.

--- vappio-install/recipes/clovrdashboard.sh
+++ vappio-install/recipes/clovrdashboard.sh
@@ -4,4 +4,5 @@ export VAPPIO_HOME=/opt
 export PYTHONPATH=$PYTHONPATH:$VAPPIO_HOME/vappio-py
 source /opt/vappio-scripts/clovrEnv.sh
 updateAllDirs.py --clovr-www
+cp -f /var/www/clovr/redirect.html /var/www/index.html
 /etc/init.d/apache2 reload
