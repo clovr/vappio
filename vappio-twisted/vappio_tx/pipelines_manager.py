5fcbe81eb05b56f88179fc0d1ac1494f3c77108f
orbitz <orbitz@gmail.com>
2011-05-17 20:29:07 +0000

    Fixing task not failing if run fails

--- vappio-twisted/vappio_tx/pipelines_manager.py
+++ vappio-twisted/vappio_tx/pipelines_manager.py
@@ -251,6 +251,8 @@ def handleWWWRunPipeline(request):
             d.addCallback(lambda p : _runPipeline(request, p))
             d.addCallback(lambda p : persist.savePipeline(p).addCallback(lambda _ : p))
             d.addCallback(lambda p : _monitor(request, p))
+            d.addErrback(lambda f : tasks_tx.updateTask(pipeline.taskName,
+                                                        lambda t : t.setState(tasks_tx.task.TASK_FAILED)))
 
             if parentPipeline:
                 parentPipeline = parentPipeline.update(children=parentPipeline.children + [('local',
