f2f52db87602c32e1b25f3d0bbd8359e70110f71
Sam Angiuoli <angiuoli@fake.com>
2011-05-18 20:36:55 +0000

    added --force-yes

--- vappio-install/recipes/rpkg.sh
+++ vappio-install/recipes/rpkg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # base code
-apt-get -y install r-base
-apt-get -y install r-base-dev
+apt-get --force-yes -y install r-base
+apt-get --force-yes -y install r-base-dev
 
 # Need to install gplots package and RColorBrewer pkg
 echo "install.packages(c(\"gplots\", \"RColorBrewer\") , repos=\"http://www.ibiblio.org/pub/languages/R/CRAN\")" | R --save --restore
