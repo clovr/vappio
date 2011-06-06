8db1129e9bab230e1cf293878f0c17d8f398fead
James White <jamesrwhite@fake.com>
2011-06-06 17:55:47 +0000

    updated R CRAN repository for installs

--- vappio-install/recipes/rpkg.sh
+++ vappio-install/recipes/rpkg.sh
@@ -5,7 +5,7 @@ apt-get --force-yes -y install r-base
 apt-get --force-yes -y install r-base-dev
 
 # Need to install gplots package and RColorBrewer pkg
-echo "install.packages(c(\"gplots\", \"RColorBrewer\") , repos=\"http://www.ibiblio.org/pub/languages/R/CRAN\")" | R --save --restore
+echo "install.packages(c(\"gplots\", \"RColorBrewer\") , repos=\"http://lib.stat.cmu.edu/R/CRAN/\")" | R --save --restore
 
 echo "source(\"http://bioconductor.org/biocLite.R\");
 biocLite(\"DESeq\")" | R --save --restore
