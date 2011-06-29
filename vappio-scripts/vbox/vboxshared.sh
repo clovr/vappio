9ed94cbad114cab0ed9e27bb8df5f49f0c30dda4
Sam Angiuoli <angiuoli@fake.com>
2011-06-29 17:50:50 +0000

    fixes to shared folders for vdi

--- vappio-scripts/vbox/vboxshared.sh
+++ vappio-scripts/vbox/vboxshared.sh
@@ -11,7 +11,5 @@ vlog "###"
 source $vappio_scripts/vbox/vbox_config.sh
 
 #Add shared folder 
-pushd /mnt
-svn export --force https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/img-conf/mnt/shared/ /mnt/
-chmod 777 shared
-popd
+svn export --force https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/img-conf/mnt/shared/
+
