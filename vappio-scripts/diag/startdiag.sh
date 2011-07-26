9a9e85d7d2c05d9c5f386a22d2e18c935a8fec9b
orbitz <orbitz@gmail.com>
2011-07-26 14:28:00 +0000

    Fixing which drive is mounted in DIAG2 and how userdata is processed

--- vappio-scripts/diag/startdiag.sh
+++ vappio-scripts/diag/startdiag.sh
@@ -6,10 +6,10 @@ vappio_scripts=/opt/vappio-scripts
 source $vappio_scripts/vappio_config.sh
 
 
-mount /dev/sda2 /mnt
+mount /dev/sdb /mnt
 
 # Download scripts
-export vappio_url_user_data=`cat /var/nimbus-metadata-server-url`/2008-08-08/user-data
+export vappio_url_user_data=`cat /var/nimbus-metadata-server-url/*`/latest/user-data
 mkdir -p $user_data_scripts
 curl --retry 3 --silent --show-error --fail -o $user_data_scripts/metadata $vappio_url_user_data
 chmod +x $user_data_scripts/metadata
