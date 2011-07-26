9e1ca4d795bcea62d3904a9fb7c017222dc3f903
orbitz <orbitz@gmail.com>
2011-07-26 14:32:15 +0000

    Updating the nimbus recipe for the proper version of ec2 api

--- vappio-install/recipes/nimbus.sh
+++ vappio-install/recipes/nimbus.sh
@@ -4,7 +4,7 @@ wget -P /tmp http://www.nimbusproject.org/downloads/install-cert-1.tar.gz
 tar -C /opt -zxvf /tmp/install-cert-1.tar.gz
 rm /tmp/install-cert-1.tar.gz
 
-wget -P /tmp http://s3.amazonaws.com/ec2-downloads/ec2-api-tools-1.3-42584.zip
-(cd /opt && unzip /tmp/ec2-api-tools-1.3-42584.zip)
-rm /tmp/ec2-api-tools-1.3-42584.zip
+wget -P /tmp http://s3.amazonaws.com/ec2-downloads/ec2-api-tools-1.3-57419.zip
+(cd /opt && unzip /tmp/ec2-api-tools-1.3-57419.zip)
+rm /tmp/ec2-api-tools-1.3-57419.zip
 
