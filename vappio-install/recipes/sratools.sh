c44c45ff05f692478c4245d191bf0a83e3e5a2f5
Dave Riley <daveriley@fake.com>
2011-08-09 21:13:53 +0000

    Now installing the 64bit version of sratools.

--- vappio-install/recipes/sratools.sh
+++ vappio-install/recipes/sratools.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-apt-get install ia32-libs
+#apt-get install ia32-libs
 
-wget -P /tmp  http://trace.ncbi.nlm.nih.gov/Traces/sra/static/sratoolkit.2.1.2-ubuntu32.tar.gz
+wget -P /tmp http://trace.ncbi.nlm.nih.gov/Traces/sra/static/sratoolkit.2.1.2-centos_linux64.tar.gz
 
-tar -C /opt/opt-packages/ -xf /tmp/sratoolkit.2.1.2-ubuntu32.tar.gz
+#wget -P /tmp  http://trace.ncbi.nlm.nih.gov/Traces/sra/static/sratoolkit.2.1.2-ubuntu32.tar.gz
 
-mv /opt/opt-packages/sratoolkit.2.1.2-ubuntu32 /opt/opt-packages/sratoolkit-2.1.2
+tar -C /opt/opt-packages/ -xf /tmp/sratoolkit.2.1.2-centos_linux64.tar.gz
+#tar -C /opt/opt-packages/ -xf /tmp/sratoolkit.2.1.2-ubuntu32.tar.gz
 
-rm -r /tmp/sratoolkit.2.1.2-ubuntu32.tar.gz
+mv /opt/opt-packages/sratoolkit.2.1.2-centos_linux64 /opt/opt-packages/sratoolkit-2.1.2
+#mv /opt/opt-packages/sratoolkit.2.1.2-ubuntu32 /opt/opt-packages/sratoolkit-2.1.2
+rm /tmp/sratoolkit.2.1.2-centos_linux64.tar.gz
+#rm -r /tmp/sratoolkit.2.1.2-ubuntu32.tar.gz
 
