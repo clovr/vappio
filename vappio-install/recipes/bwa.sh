0249bdd7eb2dc5313515b244cbd61383bf1b64fd
Dave Riley <daveriley@fake.com>
2011-08-11 22:22:49 +0000

    Switched bwa install to use version from SF instead of package manager.

--- vappio-install/recipes/bwa.sh
+++ vappio-install/recipes/bwa.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-apt-get install bwa
+wget -c -P /tmp/ http://sourceforge.net/projects/bio-bwa/files/bwa-0.5.9.tar.bz2
+
+tar -C /tmp -xjvf /tmp/bwa-0.5.9.tar.bz2
+
+cd /tmp/bwa-0.5.9
+make
+cp /tmp/bwa-0.5.9/bwa /usr/local/bin/
+
