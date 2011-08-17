74e66da47707b774170cffd65b73c0c1044b5afa
Sam Angiuoli <angiuoli@fake.com>
2011-08-17 15:43:27 +0000

    removed vappio-conf from mnt.tgz

--- vappio-install/recipes/clovrsetup.sh
+++ vappio-install/recipes/clovrsetup.sh
@@ -5,6 +5,8 @@ rm -rf $tmpdir
 mkdir $tmpdir
 
 echo p | svn export --force  https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/img-conf/mnt/shared/ $tmpdir/mnt/
+#Must not install vappio-conf on ec2
+rm -rf $tmpdir/mnt/vappio-conf
 
 pushd $tmpdir/mnt
 tar cvzf /opt/mnt.tgz .
