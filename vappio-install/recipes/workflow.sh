8819f87911696d6fb14181d037423e74c4dbbf69
Sam Angiuoli <angiuoli@fake.com>
2011-11-11 22:19:42 +0000

    fix to catch more workflow exceptions

--- vappio-install/recipes/workflow.sh
+++ vappio-install/recipes/workflow.sh
@@ -7,8 +7,9 @@
 #wget http://cb2.igs.umaryland.edu/wf_clovr_3.0vp4.tgz
 #v5 add dir.exists() to handle mkdir bug #655
 #v5.1 adds fix for premature idfile exception
-wget http://bioifx.org/wf_clovr_3.0vp5.1.tgz
-tar -C / -xvzf wf_clovr_3.0vp5.1.tgz
+#v5.2 catches exceptions during monitoring
+wget http://bioifx.org/wf_clovr_3.0vp5.2.tgz
+tar -C / -xvzf wf_clovr_3.0vp5.2.tgz
 chmod 777 /opt/workflow-sforge/idfile
 chown www-data:www-data /opt/workflow-sforge/idfile
 
