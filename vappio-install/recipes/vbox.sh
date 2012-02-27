0aa0ba163ad9e2dc655ceb147cb389d088402c4e
Sam Angiuoli <angiuoli@fake.com>
2012-02-27 16:45:11 +0000

    skipping vbox tools installation during build

--- vappio-install/recipes/vbox.sh
+++ vappio-install/recipes/vbox.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
 
-wget -c -P /tmp http://cb2.igs.umaryland.edu/vboxtools-install.tgz
 
-tar -C / -xvzf /tmp/vboxtools-install.tgz
+installvboxtools=$1
 
+if [ "$installvboxtools" == "yes" ]
+then
+    wget -c -P /tmp http://cb2.igs.umaryland.edu/vboxtools-install.tgz
+    
+    tar -C / -xvzf /tmp/vboxtools-install.tgz
+    
 #Need to fake out uname so we compile for any kernel we want to run in vbox
-mv /bin/uname /bin/uname.orig
-echo p | svn export --force https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/util/fakeuname.sh /tmp/fakeuname.sh
-cp /tmp/fakeuname.sh /bin/uname
+    mv /bin/uname /bin/uname.orig
+    echo p | svn export --force https://vappio.svn.sourceforge.net/svnroot/vappio/trunk/util/fakeuname.sh /tmp/fakeuname.sh
+    cp /tmp/fakeuname.sh /bin/uname
 #For installing the kernel modules
-apt-get -y install build-essential
-apt-get -y install linux-headers-`uname -r`
-apt-get -y install linux-image-`uname -r`
-
+    apt-get -y install build-essential
+    apt-get -y install linux-headers-`uname -r`
+    apt-get -y install linux-image-`uname -r`
+    
 #
 #Originally pulled installer from mount /dev/cdrom1 /mnt
 #MUST USE SUDO or something strange happens
-sudo bash /tmp/VBoxLinuxAdditions-amd64.run
-depmod -a
-mv /bin/uname.orig /bin/uname
-update-rc.d -f vboxadd-x11 remove
-update-rc.d -f vboxadd-service remove
-update-rc.d -f vboxadd remove
-update-rc.d -f vboxdrv remove
-
-apt-get -y install virtualbox-ose-fuse
+    sudo bash /tmp/VBoxLinuxAdditions-amd64.run
+    depmod -a
+    mv /bin/uname.orig /bin/uname
+    update-rc.d -f vboxadd-x11 remove
+    update-rc.d -f vboxadd-service remove
+    update-rc.d -f vboxadd remove
+    update-rc.d -f vboxdrv remove
+    
+    apt-get -y install virtualbox-ose-fuse
+fi
 
 #Remove 70-persistent-net.rules on shutdown to prevent networking problems on subsequent boot
 echo "rm -f /etc/udev/rules.d/70-persistent-net.rules" > /etc/init.d/rmudevnet
