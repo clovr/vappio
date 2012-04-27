5cedfbcf8458f419d0b0349bb710240b18d466e1
Sam Angiuoli <angiuoli@fake.com>
2012-04-27 21:50:20 +0000

    avoid self-kill, reschedule error queue

--- vappio-scripts/reaper.sh
+++ vappio-scripts/reaper.sh
@@ -15,6 +15,7 @@ source $vappio_scripts/vappio_config.sh
 #On testing, we've encountered state combinations including u,du,uo,duo
 echo -n "REAPER " 
 date
+myhostname=`hostname -f`
 echo "Checking for dead hosts"
 deadhosts1=`$SGE_ROOT/bin/$ARCH/qhost -q -j -xml | xpath -e "//queue/queuevalue[@name='state_string'][contains(text(),'u')]/../../@name" | perl -ne '($host) = ($_ =~ /name="(.*)"/);print $host," " if ($host)'`
 if [ "$deadhosts1" != "" ]
@@ -26,18 +27,30 @@ fi
 #
 for deadhostname in $deadhosts1
 do
-    vlog "Running remove_sgehost on $deadhostname"
-    echo "Running remove_sgehost on $deadhostname"
-    $vappio_scripts/remove_sgehost.sh $deadhostname
-    vlog "Running vp-terminate-instances on $deadhostname"
-    echo "Running vp-terminate-instances on $deadhostname"
-    vp-terminate-instances -t --by=private_dns $deadhostname
+    if [ "$deadhostname" == "$myhostname" ]
+    then
+	vlog "ERROR: reaper detected self ($deadhostname) as unresponsive, ignoring"
+    else
+	vlog "Running remove_sgehost on $deadhostname"
+	echo "Running remove_sgehost on $deadhostname"
+	$vappio_scripts/remove_sgehost.sh $deadhostname
+	vlog "Running vp-terminate-instances on $deadhostname"
+	echo "Running vp-terminate-instances on $deadhostname"    
+	vp-terminate-instances -t --by=private_dns $deadhostname
+    fi
 done
 
 #Clear all error states, task #315
 #Disabling, to avoid unintentional rescheduling of bad responding DIAG hosts
 #qmod -cj '*'
 #qmod -cq '*'
+#Check for error state and reschedule
+execqerror=`qstat -qs E -q exec.q -explain E`
+if [ "$execqerror" != "" ]
+then
+    verror "Resetting exec.q error state: $execqerror"
+    qmod -cq exec.q
+fi
 
 #Gather list of other exec hosts that are unreachable
 #hosts2=`vp-describe-cluster | grep EXEC`
