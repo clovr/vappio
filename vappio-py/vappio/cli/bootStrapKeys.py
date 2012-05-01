50802583ffea3ac17e7f1480ea3578a6ce380f6f
orbitz <orbitz@gmail.com>
2012-05-01 19:36:55 +0000

    Removing unused code, adding mkdir and chown/chmod

--- vappio-py/vappio/cli/bootStrapKeys.py
+++ vappio-py/vappio/cli/bootStrapKeys.py
@@ -9,64 +9,11 @@ from igs.utils.functional import identity
 
 from igs.utils.commands import runSystemEx, runSingleProgramEx
 
-from vappio.ec2.control import listKeypairs, addKeypair, listGroups, addGroup, authorizeGroup
-
 OPTIONS = [
-    ('cert', '-c', '--cert', 'Path to cert', identity),
-    ('pk', '-p', '--pk', 'Path to private key', identity),
     ('devel', '-d', '--devel', 'Path to devel1.pem', identity)
     ]
 
 def main(options, _args):
-    if options('general.cert') and options('general.pk'):
-        runSystemEx('cp %s /tmp/ec2-cert.pem' % options('general.cert'))
-        runSystemEx('cp %s /tmp/ec2-pk.pem' % options('general.pk'))
-        runSystemEx('chmod +r /tmp/*.pem')
-        if 'vappio_00' not in listKeypairs():
-            addKeypair('vappio_00')
-
-        groups = [g[0] for g in listGroups()]
-        if 'vappio' not in groups:
-            print 'We need to setup your EC2 group information.  In order to do this we need',
-            print 'your Amazon account number.  This is a number that is around 12 digits in length.',
-            print 'We are not recording this value in any way, simply using it to properly create',
-            print 'security groups'
-            accountNumber = raw_input('Account number: ')
-            ##
-            # Setup vappio group
-            addGroup('vappio', 'Ports for internal comm between vappio hosts')
-            authorizeGroup('vappio',
-                           'tcp',
-                           (1, 65535),
-                           'vappio',
-                           accountNumber)
-            authorizeGroup('vappio',
-                           'udp',
-                           (1, 65535),
-                           'vappio',
-                           accountNumber)
-            authorizeGroup('vappio',
-                           'icmp',
-                           (-1, -1),
-                           'vappio',
-                           accountNumber)
-            authorizeGroup('vappio',
-                           'tcp',
-                           22)
-
-        if 'web' not in groups:
-            ##
-            # Setup web group
-            addGroup('web', 'Public ports')
-            for port in [22, 80, 443, 1555, 2222, 8004, 8080, 50030, 50070]:
-                authorizeGroup('web',
-                               'tcp',
-                               port)
-    else:
-        print 'This image will not be EC2 enabled (you must specify --cert and --pk to enable it).'
-        print 'See --help for more information'
-
-        
     if options('general.devel'):
         if not os.path.exists('/mnt/keys/devel1.pem'):
             runSystemEx('cp %s /mnt/keys' % options('general.devel'))
@@ -78,13 +25,16 @@ def main(options, _args):
     runSingleProgramEx('ssh-keygen -y -f /mnt/keys/devel1.pem', keyData.append, sys.stderr.write)
     keyData = ''.join(keyData)
 
-    for path in ['/root', '/home/www-data']:
+    for path, user in [('/root', 'root'), ('/home/www-data', 'www-data')]:
         authorizedKeysPath = os.path.join(path, '.ssh', 'authorized_keys')
+        runSingleProgramEx('mkdir -p %s' % os.path.basename(authorizedKeysPath), None, None)
         authorizedKeys = open(authorizedKeysPath).read()
         if keyData not in authorizedKeys:
             fout = open(authorizedKeysPath, 'a')
             fout.write('\n' + keyData + '\n')
             fout.close()
+            runSingleProgramEx('chown %s:%s ' + authorizedKeysPath, None, None)
+            runSingleProgramEx('chmod 600 ' + authorizedKeysPath, None, None)
 
     print
     print
