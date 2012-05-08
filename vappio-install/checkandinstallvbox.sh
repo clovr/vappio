a5aad67698e0bbb0fada7a29650ace67dbdb303d
Sam Angiuoli <angiuoli@fake.com>
2012-05-08 14:13:24 +0000

    fix to check for network before vbox tools install

--- vappio-install/checkandinstallvbox.sh
+++ vappio-install/checkandinstallvbox.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-ls -L /etc/vappio/vbox/S05-vboxadd
+ipaddr=`/sbin/ifconfig | grep "inet addr" | grep -v "127.0.0.1" | awk '{ print $2 }' | awk -F: '{ print ""$2"" }'`
+if [ "$ipaddr" != "" ] 
+    ls -L /etc/vappio/vbox/S05-vboxadd
 
-if [ "$?" != 0 ]
-then
-    /opt/vappio-install/recipes/vbox.sh yes
+    if [ "$?" != 0 ]
+    then
+	ls /etc/vappio/vbox/S05-vboxadd
+	if [ "$?" != 0 ]
+	then
+	    /opt/vappio-install/recipes/vbox.sh yes
+	fi
+    fi
 fi
+
