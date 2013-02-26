0b0414dc842fd4f00b7b458ac47fad1bb0f9c81c
Dave Riley <daveriley@fake.com>
2013-02-26 22:57:11 +0000

    Added vappio-conf to shared folders.

--- vappio-scripts/vmware/vmwaremounts.sh
+++ vappio-scripts/vmware/vmwaremounts.sh
@@ -20,15 +20,19 @@ do_start() {
 
     mkdir -p $userdata_mp
     mkdir -p $keysdir
+    mkdir -p $vappioconf_mp
     chmod 777 $userdata_mp
     chmod 777 $keysdir
+    chmod 777 $vappioconf_mp
+
     mount -o ttl=3 -t vmhgfs .host:$userdata_dir $userdata_mp -o uid=33 -o gid=33 -o fmask=000 -o dmask=000
     mount -o ttl=3 -t vmhgfs .host:/keys $keysdir -o uid=33 -o gid=33 -o fmask=077 -o dmask=077
+    mount -o ttl=3 -t vmhgfs .host:$vappioconf_dir $vappioconf_mp -o uid=33 -o gid=33 -o fmask=000 -o dmask=000
  
     chmod 777 $shared_mp
     chmod 777 $userdata_mp
     chmod 777 $keysdir
-
+    chmod 777 $vappioconf_mp
     
     grep "^postgres" /etc/passwd
     if [ $? = 0 ]
@@ -42,6 +46,7 @@ do_start() {
 do_stop() {
     umount $userdata_mp
     umount $keysdir
+    umount $vappioconf_mp
     grep "^postgres" /etc/passwd
     if [ $? = 0 ]
     then
