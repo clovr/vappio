c49fb3cd58cec1064fc665c44db23d51dcdddda3
Dave Riley <daveriley@fake.com>
2013-03-26 19:28:35 +0000

    Added the Cornell repo. for pulling parts of biolinux. Will need to figure out what to do later.

--- vappio-install/recipes/biolinux.sh
+++ vappio-install/recipes/biolinux.sh
@@ -3,6 +3,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo add-apt-repository "deb http://nebc.nerc.ac.uk/bio-linux/ unstable bio-linux"
+
+# This is a hack to keep things working. It looks like the biolinux people are abandoning
+# these older versions.
+sudo add-apt-repository "deb http://packages.sgn.cornell.edu/apt/ sgn main"
 apt-get update
 apt-get --force-yes -y install bio-linux-keyring
 apt-get update
