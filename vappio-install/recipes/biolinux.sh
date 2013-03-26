97b9f638e5bc0acc60cbb8cf347cbe1891baba05
Dave Riley <daveriley@fake.com>
2013-03-26 19:33:41 +0000

    Replaced bio-linux-base-directories with bio-linux-shared.

--- vappio-install/recipes/biolinux.sh
+++ vappio-install/recipes/biolinux.sh
@@ -12,5 +12,5 @@ apt-get --force-yes -y install bio-linux-keyring
 apt-get update
 
 #TODO, consider merging with Ubuntu repository, see recipe ubuntuifxpkgs
-apt-get -y --force-yes install bio-linux-base-directories bio-linux-bldp-files bio-linux-glimmer3 bio-linux-hmmer bio-linux-trnascan 
+apt-get -y --force-yes install bio-linux-shared bio-linux-bldp-files bio-linux-glimmer3 bio-linux-hmmer bio-linux-trnascan 
 
