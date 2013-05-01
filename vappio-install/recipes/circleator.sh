99a0c3f2feff80606e81c3b74b427582cd450928
Dave Riley <daveriley@fake.com>
2013-05-01 19:40:45 +0000

    Updated circleator.sh to pull the newest release.

--- vappio-install/recipes/circleator.sh
+++ vappio-install/recipes/circleator.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 cpan -i Text::CSV
-wget -N -P /opt/opt-packages/ http://cb2.igs.umaryland.edu/circleator-1.0alpha5.tgz
-tar xzvf /opt/opt-packages/circleator-1.0alpha5.tgz -C /opt/opt-packages/
-ln -sf /opt/opt-packages/circleator-1.0alpha5 /opt/opt-packages/circleator
-ln -sf /opt/opt-packages/circleator-1.0alpha5 /opt/circleator
-rm /opt/opt-packages/circleator-1.0alpha5.tgz
+wget -N -P /opt/opt-packages/ http://cb2.igs.umaryland.edu/circleator-1.0alpha8.tar.gz
+tar -xzvf /opt/opt-packages/circleator-1.0alpha8.tar.gz -C /opt/opt-packages/
+ln -sf /opt/opt-packages/circleator-1.0alpha8 /opt/opt-packages/circleator
+ln -sf /opt/opt-packages/circleator-1.0alpha8 /opt/circleator
+rm /opt/opt-packages/circleator-1.0alpha8.tar.gz
