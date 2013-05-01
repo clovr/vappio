621b8557a0438d75a510da4083c2a1e3d585332f
Cesar Arze <cesar.arze@gmail.com>
2013-05-01 18:56:55 +0000

    Fixing missing pip command

--- vappio-install/recipes/pypi.sh
+++ vappio-install/recipes/pypi.sh
@@ -2,7 +2,7 @@
 
 ## Install pip
 ## Moved to vappiopkgs so we don't get errors during build process
-#apt-get -y install python-pip
+apt-get -y install python-pip
 
 ## Install python modules
 pip install HTSeq
