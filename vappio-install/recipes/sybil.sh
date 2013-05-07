52a87fde29a6a45e9c51b24508f5d5fa4b5a9b26
Dave Riley <daveriley@fake.com>
2013-05-07 07:43:08 +0000

    Edited the sybil install to postpone postgres starting till boot.

--- vappio-install/recipes/sybil.sh
+++ vappio-install/recipes/sybil.sh
@@ -8,4 +8,4 @@ wget -N -P /mnt/ http://sybil.igs.umaryland.edu/configure_sybil.pl
 
 #ssh -oNoneSwitch=yes -oNoneEnabled=yes -o PasswordAuthentication=no -o StrictHostKeyChecking=no -i /mnt/keys/devel1.pem root@localhost "perl /mnt/configure_sybil.pl --username=staphuser --password=staphuser123 --db_name=staph_aureus_v1 --db_url=http://sybil.igs.umaryland.edu/staph_aureus_v1.dump.gz --config_url=http://sybil.igs.umaryland.edu/staph_aureus_clovr_v1.conf --root_dir=/mnt/" &> /mnt/configure_sybil.log
 
-perl /mnt/configure_sybil.pl --start_mongo --install_sybil --install_postgres --root_dir=/mnt/ &> /mnt/configure_sybil.log
+perl /mnt/configure_sybil.pl --install_mongo --install_sybil --install_postgres --root_dir=/mnt/ &> /mnt/configure_sybil.log
