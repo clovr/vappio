cff779059b667c6996c2eae047fb3c356c7dc948
Cesar Arze <cesar.arze@gmail.com>
2014-04-10 18:47:08 +0000

    More fixes to ftp recipe

--- vappio-install/recipes/ftp.sh
+++ vappio-install/recipes/ftp.sh
@@ -10,9 +10,14 @@ useradd -g ftp -d /dev/null -s /etc ftpuser
 apt-get -y install pure-ftpd
 
 # Create a temporary user to instantiate pure-ftp'd virtual user DB
-echo "Setting up ftp users..."
-PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-(echo ${PASSWORD}; echo ${PASSWORD}) | pure-pw useradd clovr -d /mnt/user_data/ -u ftpuser
+echo "Generating random password..."
+PASSWORD=`python -c "import uuid; id = uuid.uuid4(); print str(id).upper().replace('-', '')[0:10]"`
+echo -e "${PASSWORD}\n${PASSWORD}" >> /mnt/clovr/runtime/ftp_passwd
+
+echo "Creating pure-ftpd virtual user..."
+pure-pw useradd clovr -d /mnt/user_data/ -u ftpuser -m < /mnt/clovr/runtime/ftp_passwd
+
+echo "Writing pure-ftpd database..."
 pure-pw mkdb /etc/pure-ftpd/pureftpd.pdb
 
 # Configure pure-ftpd to use virtual users
@@ -22,3 +27,6 @@ echo 'no' > PAMAuthentication
 echo 'no' > UnixAuthentication
 echo '/etc/pure-ftpd/pureftpd.pdb' > PureDB
 ln -s ../conf/PureDB /etc/pure-ftpd/auth/50pure
+
+# Shut down ftp server; should run only when we have cloud-only mode
+/etc/init.d/pure-ftpd stop
