e07c1c724f12d3cc895ba5568f3b111e5a7ab656
Cesar Arze <cesar.arze@gmail.com>
2014-04-10 16:24:09 +0000

    Re-working standard bundle attempting to fix ftp recipe bug

--- vappio-install/recipes/ftp.sh
+++ vappio-install/recipes/ftp.sh
@@ -10,11 +10,13 @@ useradd -g ftp -d /dev/null -s /etc ftpuser
 apt-get -y install pure-ftpd
 
 # Create a temporary user to instantiate pure-ftp'd virtual user DB
+echo "Setting up ftp users..."
 PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-(echo ${PASSWORD}; echo ${PASSWORD}) | pure-pw useradd test -d /mnt/user_data/ -u ftpuser
+(echo ${PASSWORD}; echo ${PASSWORD}) | pure-pw useradd clovr -d /mnt/user_data/ -u ftpuser
 pure-pw mkdb /etc/pure-ftpd/pureftpd.pdb
 
 # Configure pure-ftpd to use virtual users
+echo "Configuring pure-ftpd virtual users"
 cd /etc/pure-ftpd/conf
 echo 'no' > PAMAuthentication
 echo 'no' > UnixAuthentication
