d21399226a9b6b37d144b5e9ec4610753ff7c90c
Cesar Arze <cesar.arze@gmail.com>
2014-04-23 17:51:51 +0000

    Updating sybil recipe to include missing dependency for atypical nucleotide graphs

--- vappio-install/recipes/sybil.sh
+++ vappio-install/recipes/sybil.sh
@@ -10,3 +10,14 @@ echo p | svn export --force https://svn.code.sf.net/p/ergatis/code/trunk/src/per
 #ssh -oNoneSwitch=yes -oNoneEnabled=yes -o PasswordAuthentication=no -o StrictHostKeyChecking=no -i /mnt/keys/devel1.pem root@localhost "perl /mnt/configure_sybil.pl --username=staphuser --password=staphuser123 --db_name=staph_aureus_v1 --db_url=http://sybil.igs.umaryland.edu/staph_aureus_v1.dump.gz --config_url=http://sybil.igs.umaryland.edu/staph_aureus_clovr_v1.conf --root_dir=/mnt/" &> /mnt/configure_sybil.log
 
 perl /mnt/configure_sybil.pl --install_mongo --install_sybil --install_postgres --root_dir=/mnt/ &> /mnt/configure_sybil.log
+
+# Grab the selfsim software which is needed to generate atypical nucleotide composition graphs
+wget -O /tmp/selfsim.tgz https://dl.dropboxusercontent.com/u/15490934/selfsim.tgz
+
+cd /tmp/
+tar xfv /tmp/selfsim.tgz
+mv /tmp/selfsim /opt/opt-packages/selfsim-0.1
+ln -s /opt/opt-packages/selfsim-0.1/selfsim.bin /usr/local/bin/selfsim
+
+rm /tmp/selfsim.tgz
+
