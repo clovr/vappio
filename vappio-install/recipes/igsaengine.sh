59770fda9a7eb31f66e98958e4a4510b23c536d9
Cesar Arze <cesar.arze@gmail.com>
2014-06-20 20:58:48 +0000

    Switching to sourceforge for cdbfasta and related resources try #2

--- vappio-install/recipes/igsaengine.sh
+++ vappio-install/recipes/igsaengine.sh
@@ -11,9 +11,9 @@ make
 cp praze /usr/local/bin/
 
 #wget -P /tmp ftp://occams.dfci.harvard.edu/pub/bio/tgi/software/tgicl/tgi_cpp_library.tar.gz
-wget -P /tmp https://downloads.sourceforge.net/project/cdbfasta/tgi_cpp_library.tar.gz?r=http%3A%2F%2Fen.sourceforge.jp%2Fprojects%2Fsfnet_cdbfasta%2Fdownloads%2Ftgi_cpp_library.tar.gz%2F&ts=1403294073&use_mirror=iweb
+wget -P /tmp "https://downloads.sourceforge.net/project/cdbfasta/tgi_cpp_library.tar.gz?r=http%3A%2F%2Fen.sourceforge.jp%2Fprojects%2Fsfnet_cdbfasta%2Fdownloads%2Ftgi_cpp_library.tar.gz%2F&ts=1403294073&use_mirror=iweb"
 #wget -P /tmp ftp://occams.dfci.harvard.edu/pub/bio/tgi/software/cdbfasta/cdbfasta.tar.gz
-wget -P /tmp https://downloads.sourceforge.net/project/cdbfasta/cdbfasta.tar.gz?r=http%3A%2F%2Fen.sourceforge.jp%2Fprojects%2Fsfnet_cdbfasta%2Fdownloads%2Fcdbfasta.tar.gz%2F&ts=1403294300&use_mirror=iweb
+wget -P /tmp "https://downloads.sourceforge.net/project/cdbfasta/cdbfasta.tar.gz?r=http%3A%2F%2Fen.sourceforge.jp%2Fprojects%2Fsfnet_cdbfasta%2Fdownloads%2Fcdbfasta.tar.gz%2F&ts=1403294300&use_mirror=iweb"
 tar -C /tmp -xvzf /tmp/cdbfasta.tar.gz
 tar -C /tmp/cdbfasta -xvzf /tmp/tgi_cpp_library.tar.gz
 cd /tmp/cdbfasta
