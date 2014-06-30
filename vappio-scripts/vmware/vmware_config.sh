69b08f679e8fee61ba5564da9d2f55e0a91abb1a
Cesar Arze <cesar.arze@gmail.com>
2014-06-30 17:56:49 +0000

    Adding in missing config options for sybil install

--- vappio-scripts/vmware/vmware_config.sh
+++ vappio-scripts/vmware/vmware_config.sh
@@ -10,4 +10,5 @@ vappioconf_mp=/mnt/vappio-conf
 postgres_data_dir_mp=/mnt/pg_data
 postgres_data_dir=$shared_dir/pg_data
 postgres_uid=`id postgres -u`
+postgres_guid=`id postgres -g`
 postgres_data_dir_tarball=/opt/pg_data.tgz
