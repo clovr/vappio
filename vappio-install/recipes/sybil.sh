f62073e20c97a923eb6d4e01a6cf3ab371b06621
Cesar Arze <cesar.arze@gmail.com>
2014-10-03 14:50:06 +0000

    Merging in some changes from latest configure_sybil script into the hacked-up configure_sybil that will make postgres work on VMware

--- vappio-install/recipes/sybil.sh
+++ vappio-install/recipes/sybil.sh
@@ -2,7 +2,7 @@
 
 # Pull the configure script down
 #echo p | svn export --force https://svn.code.sf.net/p/ergatis/code/trunk/src/perl/configure_sybil.pl /mnt/configure_sybil.pl
-wget -N -P /mnt/ https://dl.dropboxusercontent.com/u/15490934/configure_sybil.working.pl
+wget -N -P /mnt/ https://dl.dropboxusercontent.com/u/15490934/configure_sybil.pl
 #wget -N -P /mnt/ http://sybil.igs.umaryland.edu/configure_sybil.pl
 
 # Pull the new prep_directories to avoid doing the bad chmod
@@ -10,7 +10,7 @@ wget -N -P /mnt/ https://dl.dropboxusercontent.com/u/15490934/configure_sybil.wo
 
 #ssh -oNoneSwitch=yes -oNoneEnabled=yes -o PasswordAuthentication=no -o StrictHostKeyChecking=no -i /mnt/keys/devel1.pem root@localhost "perl /mnt/configure_sybil.pl --username=staphuser --password=staphuser123 --db_name=staph_aureus_v1 --db_url=http://sybil.igs.umaryland.edu/staph_aureus_v1.dump.gz --config_url=http://sybil.igs.umaryland.edu/staph_aureus_clovr_v1.conf --root_dir=/mnt/" &> /mnt/configure_sybil.log
 
-perl /mnt/configure_sybil.working.pl --start_mongo --install_sybil --install_postgres --root_dir=/mnt/ &> /mnt/configure_sybil.log
+perl /mnt/configure_sybil.pl --start_mongo --install_sybil --install_postgres --root_dir=/mnt/ &> /var/log/configure_sybil.log
 
 # Grab the selfsim software which is needed to generate atypical nucleotide composition graphs                                                                                                                                                                                   
 wget -O /tmp/selfsim.tgz https://dl.dropboxusercontent.com/u/15490934/selfsim.tgz                                                                                                                                                                                                
